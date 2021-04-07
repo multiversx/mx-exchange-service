6de14d428acd3f47d96022202233edab7dc9534f
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-04-07 23:29:52 +0300

    add DexFactory GraphQL Object
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/dex/dex.model.ts
+++ src/dex/dex.model.ts
@@ -1,4 +1,22 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field } from '@nestjs/graphql';
+
+@ObjectType()
+export class DexFactoryModel {
+  @Field()
+  address: string;
+
+  @Field()
+  pairCount: number;
+
+  @Field()
+  totalTxCount: number;
+
+  @Field()
+  totalVolume: number;
+
+  @Field()
+  totalLiquidity: number;
+}
 
 @ObjectType()
 export class PairInfoModel {
