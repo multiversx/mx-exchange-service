235195412ca980a8587577e38f647ecc15867050
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-05-05 10:44:30 +0300

    add farm SC endpoints and view functions add nftTransfer method into context service add getState for SC method into context service
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/dex/models/farm.model.ts
+++ src/dex/models/farm.model.ts
@@ -1,4 +1,20 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { TokenModel } from './pair.model';
 
 @ObjectType()
-export class FarmModel {}
+export class FarmModel {
+    @Field()
+    address: string;
+
+    @Field(() => TokenModel)
+    farmedToken: TokenModel;
+
+    @Field()
+    farmToken: TokenModel;
+
+    @Field(type => [TokenModel], { nullable: true })
+    acceptedTokens?: TokenModel[];
+
+    @Field()
+    state: string;
+}
