1be32aae2cf2a672f2813e48f0678fd6cac2aa29
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-05-31 13:55:34 +0300

    farm: Add farm statistics unit test
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { BigNumber } from 'bignumber.js';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    BigNumber.config({ EXPONENTIAL_AT: [-18, 20] });
+    BigNumber.config({ EXPONENTIAL_AT: [-30, 30] });
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT);
 }
