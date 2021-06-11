34a9cba0a7f60fe271280b52003ca717aaed814f
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-06-11 11:20:05 +0300

    price-feed: Add cron job to fetch prices and store in redis
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/services/price-feed/price-feed.module.ts
+++ src/services/price-feed/price-feed.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CacheManagerModule } from '../cache-manager/cache-manager.module';
+import { ServicesModule } from '../services.module';
 import { PriceFeedService } from './price-feed.service';
 
 @Module({
+    imports: [ServicesModule, CacheManagerModule],
     providers: [PriceFeedService],
     exports: [PriceFeedService],
 })
--- src/services/price-feed/price-feed.service.ts
+++ src/services/price-feed/price-feed.service.ts
@@ -1,20 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import axios from 'axios';
 import BigNumber from 'bignumber.js';
-import { elrondConfig } from '../../config';
+import { elrondConfig, tokensPriceData } from '../../config';
+import { CacheManagerService } from '../cache-manager/cache-manager.service';
+import { RedlockService } from '../redlock';
 
 @Injectable()
 export class PriceFeedService {
     private readonly priceFeedUrl: string;
     private readonly timeoutLimit: number;
 
-    constructor() {
+    constructor(
+        private redlockService: RedlockService,
+        private cacheService: CacheManagerService,
+    ) {
         this.priceFeedUrl = elrondConfig.elrondData;
         this.timeoutLimit = elrondConfig.timeoutLimit;
     }
 
+    @Cron(CronExpression.EVERY_30_SECONDS)
+    async cachePriceFeeds(): Promise<void> {
+        for (const priceFeed in tokensPriceData) {
+            const resource = `priceFeed.${priceFeed}`;
+            const lockExpire = 5;
+            let lock;
+
+            try {
+                lock = await this.redlockService.lockTryOnce(
+                    resource,
+                    lockExpire,
+                );
+            } catch (e) {
+                return;
+            }
+            if (lock === 0) {
+                return;
+            }
+            await this.getTokenPrice(tokensPriceData.get(priceFeed));
+        }
+    }
+
     async getTokenPrice(tokenName: string): Promise<BigNumber> {
+        const cachedData = await this.cacheService.getPriceFeed(tokenName);
+        if (!!cachedData) {
+            return cachedData.priceFeed;
+        }
         const response = await this.doGet(`latest/quotes/${tokenName}/price`);
+        this.cacheService.setPriceFeed(tokenName, {
+            priceFeed: new BigNumber(response),
+        });
         return new BigNumber(response);
     }
 
