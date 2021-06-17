9c70e82f1030de16ce02277e58f9de3c0e559fd0
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-06-17 13:22:51 +0300

    services: Add Elrond Proxy service
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/services/elrond-communication/elrond-proxy.service.ts
@@ -0,0 +1,90 @@
+import {
+    AbiRegistry,
+    Address,
+    ProxyProvider,
+    SmartContract,
+    SmartContractAbi,
+} from '@elrondnetwork/erdjs/out';
+import { Injectable } from '@nestjs/common';
+import { abiConfig, elrondConfig, scAddress } from '../../config';
+
+@Injectable()
+export class ElrondProxyService {
+    private readonly proxy: ProxyProvider;
+
+    constructor() {
+        this.proxy = new ProxyProvider(
+            elrondConfig.gateway,
+            elrondConfig.proxyTimeout,
+        );
+    }
+
+    getService(): ProxyProvider {
+        return this.proxy;
+    }
+
+    async getRouterSmartContract(): Promise<SmartContract> {
+        return this.getSmartContract(
+            scAddress.routerAddress,
+            abiConfig.router,
+            'Router',
+        );
+    }
+
+    async getPairSmartContract(pairAddress: string): Promise<SmartContract> {
+        return this.getSmartContract(pairAddress, abiConfig.pair, 'Pair');
+    }
+
+    async getWrapSmartContract(): Promise<SmartContract> {
+        return this.getSmartContract(
+            scAddress.wrappingAddress,
+            abiConfig.wrap,
+            'EgldEsdtSwap',
+        );
+    }
+
+    async getFarmSmartContract(farmAddress: string): Promise<SmartContract> {
+        return this.getSmartContract(farmAddress, abiConfig.farm, 'Farm');
+    }
+
+    async getProxyDexSmartContract(): Promise<SmartContract> {
+        return this.getSmartContract(
+            scAddress.proxyDexAddress,
+            abiConfig.proxy,
+            'ProxyDexImpl',
+        );
+    }
+
+    async getDistributionSmartContract(): Promise<SmartContract> {
+        return this.getSmartContract(
+            scAddress.distributionAddress,
+            abiConfig.distribution,
+            'Distribution',
+        );
+    }
+
+    async getLockedAssetFactorySmartContract(): Promise<SmartContract> {
+        return this.getSmartContract(
+            scAddress.lockedAssetAddress,
+            abiConfig.lockedAssetFactory,
+            'LockedAssetFactory',
+        );
+    }
+
+    private async getSmartContract(
+        contractAddress: string,
+        contractAbiPath: string,
+        contractInterface: string,
+    ): Promise<SmartContract> {
+        const abiRegistry = await AbiRegistry.load({
+            files: [contractAbiPath],
+        });
+        const abi = new SmartContractAbi(abiRegistry, [contractInterface]);
+
+        const contract = new SmartContract({
+            address: new Address(contractAddress),
+            abi: abi,
+        });
+        return contract;
+    }
+}
