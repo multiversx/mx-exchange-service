c80578a400076822a870ff02bdd86401bae9a88f
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-06-17 13:28:21 +0300

    wrap: Use new Elrond Proxy service
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/wrapping/abi-wrap.service.ts
+++ src/modules/wrapping/abi-wrap.service.ts
@@ -1,41 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { ProxyProvider, Address, SmartContract } from '@elrondnetwork/erdjs';
-import { elrondConfig, abiConfig, scAddress } from '../../config';
-import { AbiRegistry } from '@elrondnetwork/erdjs/out/smartcontracts/typesystem';
-import { SmartContractAbi } from '@elrondnetwork/erdjs/out/smartcontracts/abi';
 import { Interaction } from '@elrondnetwork/erdjs/out/smartcontracts/interaction';
+import { ElrondProxyService } from '../../services/elrond-communication/elrond-proxy.service';
 
 @Injectable()
 export class AbiWrapService {
-    private readonly proxy: ProxyProvider;
-
-    constructor() {
-        this.proxy = new ProxyProvider(
-            elrondConfig.elrondApi,
-            elrondConfig.proxyTimeout,
-        );
-    }
-
-    async getContract(): Promise<SmartContract> {
-        const abiRegistry = await AbiRegistry.load({
-            files: [abiConfig.wrap],
-        });
-        const abi = new SmartContractAbi(abiRegistry, ['EgldEsdtSwap']);
-        const contract = new SmartContract({
-            address: new Address(scAddress.wrappingAddress),
-            abi: abi,
-        });
-
-        return contract;
-    }
+    constructor(private readonly elrondProxy: ElrondProxyService) {}
 
     async getWrappedEgldTokenID(): Promise<string> {
-        const contract = await this.getContract();
+        const contract = await this.elrondProxy.getWrapSmartContract();
         const interaction: Interaction = contract.methods.getWrappedEgldTokenId(
             [],
         );
         const queryResponse = await contract.runQuery(
-            this.proxy,
+            this.elrondProxy.getService(),
             interaction.buildQuery(),
         );
         const result = interaction.interpretQueryResponse(queryResponse);
