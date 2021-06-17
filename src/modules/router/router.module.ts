6f85586a4669d30abf883aea3b6f69c88b768bc6
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-06-17 13:27:36 +0300

    router: Use new Elrond Proxy service
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/router/router.module.ts
+++ src/modules/router/router.module.ts
@@ -3,9 +3,10 @@ import { RouterService } from './router.service';
 import { RouterResolver } from './router.resolver';
 import { CacheManagerModule } from '../../services/cache-manager/cache-manager.module';
 import { ContextModule } from '../../services/context/context.module';
+import { ElrondCommunicationModule } from '../../services/elrond-communication/elrond-communication.module';
 
 @Module({
-    imports: [CacheManagerModule, ContextModule],
+    imports: [ElrondCommunicationModule, CacheManagerModule, ContextModule],
     providers: [RouterService, RouterResolver],
     exports: [RouterService],
 })
