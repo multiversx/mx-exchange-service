332b33d1b9ff687141e6abe6aa7955ccce3e8edc
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-06-25 12:23:24 +0300

    analytics: add new trading info models
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/models/analytics.model.ts
+++ src/models/analytics.model.ts
@@ -1,4 +1,29 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class PairTradingModel {
+    @Field()
+    pairAddress: string;
+    @Field()
+    volumesUSD: string;
+    @Field()
+    feesUSD: string;
+}
+
+export class FactoryTradingModel {
+    @Field()
+    totalVolumesUSD: string;
+    @Field()
+    totalFeesUSD: string;
+}
+
+@ObjectType()
+export class TradingInfoModel {
+    @Field(type => FactoryTradingModel)
+    factory: FactoryTradingModel;
+    @Field(type => [PairTradingModel])
+    pairs: PairTradingModel[];
+}
 
 @ObjectType()
 export class AnalyticsModel {}
