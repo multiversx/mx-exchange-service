d3014aff8bd54303c2a081a4e05e258753c754f7
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-06-30 17:07:12 +0300

    add Winston logging module
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,44 @@ import { ProxyModule } from './modules/proxy/proxy.module';
 import { LockedAssetModule } from './modules/locked-asset-factory/locked-asset.module';
 import { UserModule } from './modules/user/user.module';
 import { AnalyticsModule } from './modules/analytics/analytics.module';
+import {
+    utilities as nestWinstonModuleUtilities,
+    WinstonModule
+} from 'nest-winston';
+import * as winston from 'winston';
+import * as Transport from 'winston-transport';
+import { GraphQLError, GraphQLFormattedError } from 'graphql';
+
+
+const logTransports: Transport[] = [
+    new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike(),
+        )
+    })
+]
+
+const loglevel = !!process.env.LOG_LEVEL ? process.env.LOG_LEVEL: 'error';
+
+if (!!process.env.LOG_FILE) {
+    logTransports.push(
+        new winston.transports.File({
+            filename: process.env.LOG_FILE,
+            dirname: 'logs',
+            maxsize: 100000,
+            level: loglevel
+        })
+    )
+}
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
+        }),
+        WinstonModule.forRoot({
+            transports: logTransports
         }),
         GraphQLModule.forRoot({
             autoSchemaFile: 'schema.gql',
