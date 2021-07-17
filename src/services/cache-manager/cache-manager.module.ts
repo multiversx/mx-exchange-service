8b50a88747ea307ea47e518380938f45deff25ee
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-07-17 18:22:29 +0300

    cache-manager: remove unused farm cache
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/services/cache-manager/cache-manager.module.ts
+++ src/services/cache-manager/cache-manager.module.ts
@@ -6,13 +6,11 @@ import { CacheWrapService } from './cache-wrapping.service';
 import { CacheProxyService } from './cache-proxy.service';
 import { CacheProxyPairService } from './cache-proxy-pair.service';
 import { CacheProxyFarmService } from './cache-proxy-farm.service';
-import { CacheFarmService } from './cache-farm.service';
 import { CacheLockedAssetService } from './cache-locked-asset.service';
 
 @Module({
     providers: [
         CacheManagerService,
-        CacheFarmService,
         CacheDistributionService,
         CacheWrapService,
         CacheProxyService,
@@ -26,13 +24,10 @@ import { CacheLockedAssetService } from './cache-locked-asset.service';
             store: redisStore,
             host: process.env.REDIS_URL,
             port: process.env.REDIS_PORT,
-            prefix: process.env.REDIS_PREFIX,
-            // password: process.env.REDIS_PASSWORD,
         }),
     ],
     exports: [
         CacheManagerService,
-        CacheFarmService,
         CacheWrapService,
         CacheDistributionService,
         CacheProxyService,
