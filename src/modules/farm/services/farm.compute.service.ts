4daae1e76c70d0937e83d5a0e2be95e404e74d80
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-09-22 19:08:06 +0300

    farm: add method to local compute farm rewards for position
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/farm/services/farm.compute.service.ts
+++ src/modules/farm/services/farm.compute.service.ts
@@ -1,8 +1,11 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import BigNumber from 'bignumber.js';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { scAddress } from 'src/config';
 import { PairService } from 'src/modules/pair/pair.service';
+import { ContextService } from 'src/services/context/context.service';
 import { Logger } from 'winston';
+import { FarmTokenAttributesModel } from '../models/farmTokenAttributes.model';
 import { FarmGetterService } from './farm.getter.service';
 
 @Injectable()
@@ -11,6 +14,7 @@ export class FarmComputeService {
         @Inject(forwardRef(() => FarmGetterService))
         private readonly farmGetterService: FarmGetterService,
         private readonly pairService: PairService,
+        private readonly context: ContextService,
         @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
     ) {}
 
@@ -46,5 +50,72 @@ export class FarmComputeService {
             farmingTokenID,
         );
         return this.pairService.getLpTokenPriceUSD(pairAddress);
+    }
+
+    async computeFarmRewardsForPosition(
+        farmAddress: string,
+        liquidity: string,
+        decodedAttributes: FarmTokenAttributesModel,
+    ): Promise<BigNumber> {
+        const [
+            currentNonce,
+            lastRewardBlockNonce,
+            perBlockRewardAmount,
+            undistributedFees,
+            currentBlockFee,
+            divisionSafetyConstant,
+            farmTokenSupply,
+            farmRewardPerShare,
+        ] = await Promise.all([
+            this.context.getShardCurrentBlockNonce(1),
+            this.farmGetterService.getLastRewardBlockNonce(farmAddress),
+            this.farmGetterService.getRewardsPerBlock(farmAddress),
+            this.farmGetterService.getUndistributedFees(farmAddress),
+            this.farmGetterService.getCurrentBlockFee(farmAddress),
+            this.farmGetterService.getDivisionSafetyConstant(farmAddress),
+            this.farmGetterService.getFarmTokenSupply(farmAddress),
+            this.farmGetterService.getRewardPerShare(farmAddress),
+        ]);
+
+        const amountBig = new BigNumber(liquidity);
+        const currentBlockBig = new BigNumber(currentNonce);
+        const lastRewardBlockNonceBig = new BigNumber(lastRewardBlockNonce);
+        const perBlockRewardAmountBig = new BigNumber(perBlockRewardAmount);
+        const undistributedFeesBig = new BigNumber(undistributedFees);
+        const currentBlockFeeBig = new BigNumber(currentBlockFee);
+        const divisionSafetyConstantBig = new BigNumber(divisionSafetyConstant);
+        const farmTokenSupplyBig = new BigNumber(farmTokenSupply);
+        const farmRewardPerShareBig = new BigNumber(farmRewardPerShare);
+        const rewardPerShareBig = new BigNumber(
+            decodedAttributes.rewardPerShare,
+        );
+
+        let toBeMinted = new BigNumber(0);
+        const feesBig = undistributedFeesBig.plus(currentBlockFeeBig);
+
+        if (currentNonce > lastRewardBlockNonce) {
+            toBeMinted = perBlockRewardAmountBig.times(
+                currentBlockBig.minus(lastRewardBlockNonceBig),
+            );
+        }
+        const rewardIncrease = toBeMinted.plus(feesBig);
+        const rewardPerShareIncrease = rewardIncrease
+            .times(divisionSafetyConstantBig)
+            .dividedBy(farmTokenSupplyBig);
+        const futureRewardPerShare = farmRewardPerShareBig.plus(
+            rewardPerShareIncrease,
+        );
+        if (
+            futureRewardPerShare.isGreaterThan(decodedAttributes.rewardPerShare)
+        ) {
+            const rewardPerShareDiff = futureRewardPerShare.minus(
+                rewardPerShareBig,
+            );
+
+            return amountBig
+                .times(rewardPerShareDiff)
+                .dividedBy(divisionSafetyConstantBig);
+        }
+        return new BigNumber(0);
     }
 }
