6b35b59fb6ae2bee3733a73ec37cb2e40d2eb376
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-09-22 23:22:44 +0300

    elrond-communication: remove unused elrond extras service
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/services/elrond-communication/elrond-communication.module.ts
+++ src/services/elrond-communication/elrond-communication.module.ts
@@ -3,11 +3,10 @@ import { CommonAppModule } from 'src/common.app.module';
 import { CachingModule } from '../caching/cache.module';
 import { ElrondApiService } from './elrond-api.service';
 import { ElrondProxyService } from './elrond-proxy.service';
-import { ElrondExtrasApiService } from './elrond.extras.api.service';
 
 @Module({
     imports: [CommonAppModule, CachingModule],
-    providers: [ElrondProxyService, ElrondApiService, ElrondExtrasApiService],
-    exports: [ElrondProxyService, ElrondApiService, ElrondExtrasApiService],
+    providers: [ElrondProxyService, ElrondApiService],
+    exports: [ElrondProxyService, ElrondApiService],
 })
 export class ElrondCommunicationModule {}
