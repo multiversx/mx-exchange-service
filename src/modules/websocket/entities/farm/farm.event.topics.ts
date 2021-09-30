444cf51e1bacd43028dcae971382f978eb2c37f2
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-09-30 10:24:12 +0300

    events: add event name in decoded topics
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/websocket/entities/farm/farm.event.topics.ts
+++ src/modules/websocket/entities/farm/farm.event.topics.ts
@@ -1,24 +1,27 @@
 import { Address } from '@elrondnetwork/erdjs/out';
 
 export class FarmEventsTopics {
+    private eventName: string;
     private caller: Address;
     private tokenID: string;
     private lockedRewards: boolean;
     private epoch: number;
 
     constructor(rawTopics: string[]) {
-        this.caller = new Address(Buffer.from(rawTopics[0], 'base64'));
-        this.tokenID = Buffer.from(rawTopics[1], 'base64').toString();
+        this.eventName = Buffer.from(rawTopics[0], 'base64').toString();
+        this.caller = new Address(Buffer.from(rawTopics[1], 'base64'));
+        this.tokenID = Buffer.from(rawTopics[2], 'base64').toString();
         this.lockedRewards =
-            Buffer.from(rawTopics[2], 'base64').toString() === 'true';
+            Buffer.from(rawTopics[3], 'base64').toString() === 'true';
         this.epoch = parseInt(
-            Buffer.from(rawTopics[3], 'base64').toString('hex'),
+            Buffer.from(rawTopics[4], 'base64').toString('hex'),
             16,
         );
     }
 
     toPlainObject() {
         return {
+            eventName: this.eventName,
             caller: this.caller.bech32(),
             tokenID: this.tokenID,
             lockedRewards: this.lockedRewards,
--- src/modules/websocket/entities/proxy/proxy.event.topics.ts
+++ src/modules/websocket/entities/proxy/proxy.event.topics.ts
@@ -1,6 +1,7 @@
 import { Address } from '@elrondnetwork/erdjs/out';
 
 export class PairProxyTopics {
+    private eventName: string;
     private caller: Address;
     private firstTokenID: string;
     private secondTokenID: string;
@@ -8,18 +9,20 @@ export class PairProxyTopics {
     private epoch: number;
 
     constructor(rawTopics: string[]) {
-        this.firstTokenID = Buffer.from(rawTopics[0], 'base64').toString();
-        this.secondTokenID = Buffer.from(rawTopics[1], 'base64').toString();
-        this.caller = new Address(Buffer.from(rawTopics[2], 'base64'));
-        this.pairAddress = new Address(Buffer.from(rawTopics[3], 'base64'));
+        this.eventName = Buffer.from(rawTopics[0], 'base64').toString();
+        this.firstTokenID = Buffer.from(rawTopics[1], 'base64').toString();
+        this.secondTokenID = Buffer.from(rawTopics[2], 'base64').toString();
+        this.caller = new Address(Buffer.from(rawTopics[3], 'base64'));
+        this.pairAddress = new Address(Buffer.from(rawTopics[4], 'base64'));
         this.epoch = parseInt(
-            Buffer.from(rawTopics[4], 'base64').toString('hex'),
+            Buffer.from(rawTopics[5], 'base64').toString('hex'),
             16,
         );
     }
 
     toPlainObject() {
         return {
+            eventName: this.eventName,
             caller: this.caller.bech32(),
             firstTokenID: this.firstTokenID,
             secondTokenID: this.secondTokenID,
@@ -30,23 +33,26 @@ export class PairProxyTopics {
 }
 
 export class FarmProxyTopics {
+    private eventName: string;
     private tokenID: string;
     private caller: Address;
     private farmAddress: Address;
     private epoch: number;
 
     constructor(rawTopics: string[]) {
-        this.tokenID = Buffer.from(rawTopics[0], 'base64').toString();
-        this.caller = new Address(Buffer.from(rawTopics[1], 'base64'));
-        this.farmAddress = new Address(Buffer.from(rawTopics[2], 'base64'));
+        this.eventName = Buffer.from(rawTopics[0], 'base64').toString();
+        this.tokenID = Buffer.from(rawTopics[1], 'base64').toString();
+        this.caller = new Address(Buffer.from(rawTopics[2], 'base64'));
+        this.farmAddress = new Address(Buffer.from(rawTopics[3], 'base64'));
         this.epoch = parseInt(
-            Buffer.from(rawTopics[3], 'base64').toString('hex'),
+            Buffer.from(rawTopics[4], 'base64').toString('hex'),
             16,
         );
     }
 
     toPlainObject() {
         return {
+            eventName: this.eventName,
             tokenID: this.tokenID,
             caller: this.caller.bech32(),
             farmAddress: this.farmAddress.bech32(),
