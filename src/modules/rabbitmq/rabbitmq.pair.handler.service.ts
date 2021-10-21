b856366b5410f91da870ff161675be12a0f25794
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2021-10-21 21:38:00 +0300

    rabbitmq: fix reserves updates on event
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/rabbitmq/rabbitmq.pair.handler.service.ts
+++ src/modules/rabbitmq/rabbitmq.pair.handler.service.ts
@@ -1,8 +1,10 @@
 import { Inject, Injectable } from '@nestjs/common';
+import BigNumber from 'bignumber.js';
 import { RedisPubSub } from 'graphql-redis-subscriptions';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { PUB_SUB } from 'src/services/redis.pubSub.module';
 import { Logger } from 'winston';
+import { PairGetterService } from '../pair/services/pair.getter.service';
 import { PairSetterService } from '../pair/services/pair.setter.service';
 import { PAIR_EVENTS } from './entities/generic.types';
 import { AddLiquidityEvent } from './entities/pair/addLiquidity.event';
@@ -15,6 +17,7 @@ import { SwapNoFeeEvent } from './entities/pair/swapNoFee.event';
 export class RabbitMQPairHandlerService {
     private invalidatedKeys = [];
     constructor(
+        private readonly pairGetterService: PairGetterService,
         private readonly pairSetterService: PairSetterService,
         @Inject(PUB_SUB) private pubSub: RedisPubSub,
         @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
@@ -23,18 +26,40 @@ export class RabbitMQPairHandlerService {
     async handleSwapEvent(
         event: SwapFixedInputEvent | SwapFixedOutputEvent,
     ): Promise<void> {
+        const [firstTokenID, secondTokenID] = await Promise.all([
+            this.pairGetterService.getFirstTokenID(event.getAddress()),
+            this.pairGetterService.getSecondTokenID(event.getAddress()),
+        ]);
+
+        let firstTokenReserves: BigNumber;
+        let secondTokenReserves: BigNumber;
+
+        if (
+            event.getPairReserves()[0].tokenID === firstTokenID &&
+            event.getPairReserves()[1].tokenID === secondTokenID
+        ) {
+            firstTokenReserves = event.getPairReserves()[0].amount;
+            secondTokenReserves = event.getPairReserves()[1].amount;
+        } else if (
+            event.getPairReserves()[1].tokenID === firstTokenID &&
+            event.getPairReserves()[0].tokenID === secondTokenID
+        ) {
+            firstTokenReserves = event.getPairReserves()[1].amount;
+            secondTokenReserves = event.getPairReserves()[0].amount;
+        }
         this.invalidatedKeys.push(
             await this.pairSetterService.setFirstTokenReserve(
                 event.getAddress(),
-                event.getPairReserves()[0].amount.toFixed(),
+                firstTokenReserves.toFixed(),
             ),
         );
         this.invalidatedKeys.push(
             await this.pairSetterService.setSecondTokenReserve(
                 event.getAddress(),
-                event.getPairReserves()[1].amount.toFixed(),
+                secondTokenReserves.toFixed(),
             ),
         );
+
         await this.deleteCacheKeys();
 
         event instanceof SwapFixedInputEvent
@@ -49,14 +74,36 @@ export class RabbitMQPairHandlerService {
     async handleLiquidityEvent(
         event: AddLiquidityEvent | RemoveLiquidityEvent,
     ): Promise<void> {
+        const [firstTokenID, secondTokenID] = await Promise.all([
+            this.pairGetterService.getFirstTokenID(event.getAddress()),
+            this.pairGetterService.getSecondTokenID(event.getAddress()),
+        ]);
+
+        let firstTokenReserves: BigNumber;
+        let secondTokenReserves: BigNumber;
+
+        if (
+            event.getPairReserves()[0].tokenID === firstTokenID &&
+            event.getPairReserves()[1].tokenID === secondTokenID
+        ) {
+            firstTokenReserves = event.getPairReserves()[0].amount;
+            secondTokenReserves = event.getPairReserves()[1].amount;
+        } else if (
+            event.getPairReserves()[1].tokenID === firstTokenID &&
+            event.getPairReserves()[0].tokenID === secondTokenID
+        ) {
+            firstTokenReserves = event.getPairReserves()[1].amount;
+            secondTokenReserves = event.getPairReserves()[0].amount;
+        }
+
         const cacheKeys = await Promise.all([
             this.pairSetterService.setFirstTokenReserve(
                 event.getAddress(),
-                event.getPairReserves()[0].amount.toFixed(),
+                firstTokenReserves.toFixed(),
             ),
             this.pairSetterService.setSecondTokenReserve(
                 event.getAddress(),
-                event.getPairReserves()[1].amount.toFixed(),
+                secondTokenReserves.toFixed(),
             ),
             this.pairSetterService.setTotalSupply(
                 event.getAddress(),
