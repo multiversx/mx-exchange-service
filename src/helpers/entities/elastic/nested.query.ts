4a6653d2569c3ae9833954f85bc628a4cbe19382
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2022-01-04 17:43:57 +0200

    elasticsearch: accept MatchQuery array for nested query
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/helpers/entities/elastic/nested.query.ts
+++ src/helpers/entities/elastic/nested.query.ts
@@ -1,19 +1,14 @@
 import { AbstractQuery } from './abstract.query';
-import { QueryType } from './query.type';
+import { MatchQuery } from './match.query';
 
 export class NestedQuery extends AbstractQuery {
-    buildQuery(key: string, value: any): any {
+    buildQuery(key: string, value: MatchQuery[]): any {
         return {
             nested: {
                 path: key,
                 query: {
                     bool: {
-                        must: [
-                            QueryType.Match(
-                                Object.keys(value)[0],
-                                Object.values(value)[0],
-                            ),
-                        ],
+                        must: value,
                     },
                 },
             },
