838f84e2628e3577892562832d0f348caac588a1
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2022-01-11 15:55:31 +0200

    elasticsearch: change nested match from abstract to match query
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/helpers/entities/elastic/nested.query.ts
+++ src/helpers/entities/elastic/nested.query.ts
@@ -1,7 +1,8 @@
 import { AbstractQuery } from './abstract.query';
+import { MatchQuery } from './match.query';
 
 export class NestedQuery extends AbstractQuery {
-    buildQuery(key: string, value: AbstractQuery[]): any {
+    buildQuery(key: string, value: MatchQuery[]): any {
         return {
             nested: {
                 path: key,
