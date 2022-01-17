bc2e42b4f7a969772f267c1f2487d63e8f5c3c78
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2022-01-17 17:42:10 +0200

    price-discovery: add field resolvers methods
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/modules/price-discovery/price.discovery.resolver.ts
@@ -0,0 +1,103 @@
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { ApolloError } from 'apollo-server-express';
+import { EsdtToken } from 'src/models/tokens/esdtToken.model';
+import { NftCollection } from 'src/models/tokens/nftCollection.model';
+import { PriceDiscoveryModel } from './models/price.discovery.model';
+import { PriceDiscoveryGetterService } from './services/price.discovery.getter.service';
+
+@Resolver(() => PriceDiscoveryModel)
+export class PriceDiscoveryResolver {
+    constructor(
+        private readonly priceDiscoveryGetter: PriceDiscoveryGetterService,
+    ) {}
+
+    private async genericFieldResover(fieldResolver: () => any): Promise<any> {
+        try {
+            return await fieldResolver();
+        } catch (error) {
+            throw new ApolloError(error);
+        }
+    }
+
+    @ResolveField()
+    async launchedToken(
+        @Parent() parent: PriceDiscoveryModel,
+    ): Promise<EsdtToken> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getLaunchedToken(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async acceptedToken(
+        @Parent() parent: PriceDiscoveryModel,
+    ): Promise<EsdtToken> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getAcceptedToken(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async redeemToken(
+        @Parent() parent: PriceDiscoveryModel,
+    ): Promise<NftCollection> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getRedeemToken(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async lpToken(@Parent() parent: PriceDiscoveryModel): Promise<EsdtToken> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getLpToken(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async launchedTokenAmount(
+        @Parent() parent: PriceDiscoveryModel,
+    ): Promise<string> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getLaunchedTokenAmount(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async acceptedTokenAmount(
+        @Parent() parent: PriceDiscoveryModel,
+    ): Promise<string> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getAcceptedTokenAmount(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async lpTokensReceived(
+        @Parent() parent: PriceDiscoveryModel,
+    ): Promise<string> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getLpTokensReceived(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async startEpoch(@Parent() parent: PriceDiscoveryModel): Promise<number> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getStartEpoch(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async endEpoch(@Parent() parent: PriceDiscoveryModel): Promise<number> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getEndEpoch(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async pairAddress(@Parent() parent: PriceDiscoveryModel): Promise<string> {
+        return await this.genericFieldResover(() =>
+            this.priceDiscoveryGetter.getPairAddress(parent.address),
+        );
+    }
+}
