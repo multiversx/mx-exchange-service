00656e903681957400d8ea9a3f943fd3d747b14a
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2022-03-03 16:48:56 +0200

    farm: remove burned token amount
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/farm/services/abi-farm.service.ts
+++ src/modules/farm/services/abi-farm.service.ts
@@ -250,20 +250,6 @@ export class AbiFarmService {
         return response.firstValue.valueOf().name;
     }
 
-    async getBurnedTokenAmount(
-        farmAddress: string,
-        tokenID: string,
-    ): Promise<string> {
-        const [contract] = await this.elrondProxy.getFarmSmartContract(
-            farmAddress,
-        );
-        const interaction: Interaction = contract.methods.getBurnedTokenAmount([
-            BytesValue.fromUTF8(tokenID),
-        ]);
-        const response = await this.getGenericData(contract, interaction);
-        return response.firstValue.valueOf().toFixed();
-    }
-
     async getProduceRewardsEnabled(farmAddress: string): Promise<boolean> {
         const response = await this.gatewayService.getSCStorageKey(
             farmAddress,
