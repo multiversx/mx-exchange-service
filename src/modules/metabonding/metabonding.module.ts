02d4e42dc87edc54866ee846cb7ce9a6554545db
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2022-03-10 12:59:41 +0200

    metabonding: add resolver and module
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/modules/metabonding/metabonding.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { CachingModule } from 'src/services/caching/cache.module';
+import { ContextModule } from 'src/services/context/context.module';
+import { ElrondCommunicationModule } from 'src/services/elrond-communication/elrond-communication.module';
+import { MetabondingResolver } from './metabonding.resolver';
+import { MetabondingAbiService } from './services/metabonding.abi.service';
+import { MetabondingGetterService } from './services/metabonding.getter.service';
+import { MetabondingService } from './services/metabonding.service';
+import { MetabondingSetterService } from './services/metabonding.setter.service';
+import { MetabondingTransactionService } from './services/metabonding.transactions.service';
+
+@Module({
+    imports: [ElrondCommunicationModule, ContextModule, CachingModule],
+    providers: [
+        MetabondingService,
+        MetabondingAbiService,
+        MetabondingGetterService,
+        MetabondingSetterService,
+        MetabondingTransactionService,
+        MetabondingResolver,
+    ],
+})
+export class MetabondingModule {}
