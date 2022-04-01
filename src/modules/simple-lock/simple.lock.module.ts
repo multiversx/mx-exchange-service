5725ba9373eb1820f194ed274d99b56c1376088c
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2022-04-01 17:55:48 +0300

    simple-lock: add query for transactions
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/simple-lock/simple.lock.module.ts
+++ src/modules/simple-lock/simple.lock.module.ts
@@ -2,19 +2,29 @@ import { Module } from '@nestjs/common';
 import { CachingModule } from 'src/services/caching/cache.module';
 import { ContextModule } from 'src/services/context/context.module';
 import { ElrondCommunicationModule } from 'src/services/elrond-communication/elrond-communication.module';
+import { PairModule } from '../pair/pair.module';
+import { WrappingModule } from '../wrapping/wrap.module';
 import { SimpleLockAbiService } from './services/simple.lock.abi.service';
 import { SimpleLockGetterService } from './services/simple.lock.getter.service';
 import { SimpleLockService } from './services/simple.lock.service';
 import { SimpleLockSetterService } from './services/simple.lock.setter.service';
+import { SimpleLockTransactionService } from './services/simple.lock.transactions.service';
 import { SimpleLockResolver } from './simple.lock.resolver';
 
 @Module({
-    imports: [ElrondCommunicationModule, ContextModule, CachingModule],
+    imports: [
+        ElrondCommunicationModule,
+        ContextModule,
+        CachingModule,
+        PairModule,
+        WrappingModule,
+    ],
     providers: [
         SimpleLockService,
         SimpleLockAbiService,
         SimpleLockGetterService,
         SimpleLockSetterService,
+        SimpleLockTransactionService,
         SimpleLockResolver,
     ],
 })
--- src/modules/simple-lock/simple.lock.resolver.ts
+++ src/modules/simple-lock/simple.lock.resolver.ts
@@ -1,14 +1,21 @@
-import { Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { ApolloError } from 'apollo-server-express';
+import { User } from 'src/helpers/userDecorator';
+import { InputTokenModel } from 'src/models/inputToken.model';
+import { TransactionModel } from 'src/models/transaction.model';
+import { GqlAuthGuard } from '../auth/gql.auth.guard';
 import { SimpleLockModel } from './models/simple.lock.model';
 import { SimpleLockGetterService } from './services/simple.lock.getter.service';
 import { SimpleLockService } from './services/simple.lock.service';
+import { SimpleLockTransactionService } from './services/simple.lock.transactions.service';
 
 @Resolver(() => SimpleLockModel)
 export class SimpleLockResolver {
     constructor(
         private readonly simpleLockService: SimpleLockService,
         private readonly simpleLockGetter: SimpleLockGetterService,
+        private readonly simpleLockTransactions: SimpleLockTransactionService,
     ) {}
 
     private async genericFieldResover(fieldResolver: () => any): Promise<any> {
@@ -38,5 +45,60 @@ export class SimpleLockResolver {
         try {
             return this.simpleLockService.getSimpleLock();
         } catch (error) {}
+    }
+
+    @UseGuards(GqlAuthGuard)
+    @Query(() => TransactionModel)
+    async unlockTokens(
+        @Args('inputTokens') inputTokens: InputTokenModel,
+        @User() user: any,
+    ): Promise<TransactionModel> {
+        try {
+            return await this.simpleLockTransactions.unlockTokens(
+                user.publicKey,
+                inputTokens,
+            );
+        } catch (error) {
+            throw new ApolloError(error);
+        }
+    }
+
+    @UseGuards(GqlAuthGuard)
+    @Query(() => [TransactionModel])
+    async addLiquidityLockedTokenBatch(
+        @Args('inputTokens', { type: () => [InputTokenModel] })
+        inputTokens: InputTokenModel[],
+        @Args('tolerance') tolerance: number,
+        @User() user: any,
+    ): Promise<TransactionModel[]> {
+        try {
+            return await this.simpleLockTransactions.addLiquidityLockedTokenBatch(
+                user.publicKey,
+                inputTokens,
+                tolerance,
+            );
+        } catch (error) {
+            throw new ApolloError(error);
+        }
+    }
+
+    @UseGuards(GqlAuthGuard)
+    @Query(() => [TransactionModel])
+    async removeLiquidityLockedToken(
+        @Args('inputTokens') inputTokens: InputTokenModel,
+        @Args('attributes') attributes: string,
+        @Args('tolerance') tolerance: number,
+        @User() user: any,
+    ): Promise<TransactionModel[]> {
+        try {
+            return await this.simpleLockTransactions.removeLiquidityLockedToken(
+                user.publicKey,
+                inputTokens,
+                attributes,
+                tolerance,
+            );
+        } catch (error) {
+            throw new ApolloError(error);
+        }
     }
 }
