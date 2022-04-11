12661e401a247274ef76cafbf5db8efea5623896
Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>
2022-04-11 20:32:33 +0300

    rabbitmq: add price discovery events handler
    Signed-off-by: Claudiu Ion Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/modules/rabbitmq/rabbitmq.price.discovery.handler.service.ts
@@ -0,0 +1,89 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { PUB_SUB } from 'src/services/redis.pubSub.module';
+import { Logger } from 'winston';
+import { PriceDiscoveryComputeService } from '../price-discovery/services/price.discovery.compute.service';
+import { PriceDiscoverySetterService } from '../price-discovery/services/price.discovery.setter.service';
+import { PRICE_DISCOVERY_EVENTS } from './entities/generic.types';
+import { DepositEvent } from './entities/price-discovery/deposit.event';
+import { WithdrawEvent } from './entities/price-discovery/withdraw.event';
+
+@Injectable()
+export class RabbitMqPriceDiscoveryHandlerService {
+    constructor(
+        private readonly priceDiscoverySetter: PriceDiscoverySetterService,
+        private readonly priceDiscoveryCompute: PriceDiscoveryComputeService,
+        @Inject(PUB_SUB) private pubSub: RedisPubSub,
+        @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+    ) {}
+
+    async handleEvent(event: DepositEvent | WithdrawEvent): Promise<void> {
+        const [
+            priceDiscoveryAddress,
+            launchedTokenAmount,
+            acceptedTokenAmount,
+            launchedTokenPrice,
+        ] = [
+            event.getAddress(),
+            event.launchedTokenAmount.toFixed(),
+            event.acceptedTokenAmount.toFixed(),
+            event.launchedTokenPrice.toFixed(),
+        ];
+
+        let cacheKeys: string[] = await Promise.all([
+            this.priceDiscoverySetter.setLaunchedTokenAmount(
+                priceDiscoveryAddress,
+                launchedTokenAmount,
+            ),
+            this.priceDiscoverySetter.setAcceptedTokenAmount(
+                priceDiscoveryAddress,
+                acceptedTokenAmount,
+            ),
+            this.priceDiscoverySetter.setLaunchedTokenPrice(
+                priceDiscoveryAddress,
+                launchedTokenPrice,
+            ),
+            this.priceDiscoverySetter.setCurrentPhase(
+                priceDiscoveryAddress,
+                event.currentPhase,
+            ),
+        ]);
+
+        await this.deleteCacheKeys(cacheKeys);
+
+        const [acceptedTokenPrice, launchedTokenPriceUSD] = await Promise.all([
+            this.priceDiscoveryCompute.computeAcceptedTokenPrice(
+                priceDiscoveryAddress,
+            ),
+            this.priceDiscoveryCompute.computeLaunchedTokenPriceUSD(
+                priceDiscoveryAddress,
+            ),
+        ]);
+
+        cacheKeys = await Promise.all([
+            this.priceDiscoverySetter.setAcceptedTokenPrice(
+                priceDiscoveryAddress,
+                acceptedTokenPrice,
+            ),
+            this.priceDiscoverySetter.setLaunchedTokenPriceUSD(
+                priceDiscoveryAddress,
+                launchedTokenPriceUSD,
+            ),
+        ]);
+
+        await this.deleteCacheKeys(cacheKeys);
+
+        event.getIdentifier() === PRICE_DISCOVERY_EVENTS.DEPOSIT
+            ? await this.pubSub.publish(PRICE_DISCOVERY_EVENTS.DEPOSIT, {
+                  depositEvent: event,
+              })
+            : await this.pubSub.publish(PRICE_DISCOVERY_EVENTS.WITHDARW, {
+                  withdrawEvent: event,
+              });
+    }
+
+    private async deleteCacheKeys(invalidatedKeys: string[]) {
+        await this.pubSub.publish('deleteCacheKeys', invalidatedKeys);
+    }
+}
