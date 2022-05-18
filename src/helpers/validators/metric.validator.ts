ab2276f7afc602419a2fe0a9d9002ca37ec1ae53
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-05-18 16:49:55 +0300

    SERVICES-18 add input validators
    add series input validator
    add metric input validator
    add timestamp input validator
    
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/helpers/validators/metric.validator.ts
@@ -0,0 +1,57 @@
+import {
+    registerDecorator,
+    ValidationOptions,
+    ValidatorConstraint,
+    ValidatorConstraintInterface,
+} from 'class-validator';
+
+const metrics = [
+    'totalLockedValueUSD',
+    'firstTokenLocked',
+    'firstTokenLockedValueUSD',
+    'firstTokenPrice',
+    'firstTokenVolume',
+    'secondTokenLocked',
+    'secondTokenLockedValueUSD',
+    'secondTokenPrice',
+    'secondTokenVolume',
+    'lockedValueUSD',
+    'liquidity',
+    'lockedValue',
+    'lockedValueUSD',
+    'priceUSD',
+    'volume',
+    'volumeUSD',
+    'feesUSD',
+    'launchedTokenAmount',
+    'acceptedTokenAmount',
+    'launchedTokenPrice',
+    'acceptedTokenPrice',
+    'launchedTokenPriceUSD',
+    'feeBurned',
+    'penaltyBurned',
+];
+
+@ValidatorConstraint()
+export class IsValidMetricConstraint implements ValidatorConstraintInterface {
+    validate(metric: any): boolean {
+        return metrics.includes(metric);
+    }
+
+    defaultMessage() {
+        // here you can provide default error message if validation failed
+        return 'Invalid metric';
+    }
+}
+
+export function IsValidMetric(validationOptions?: ValidationOptions) {
+    return (object: Object, propertyName: string) => {
+        registerDecorator({
+            target: object.constructor,
+            propertyName: propertyName,
+            options: validationOptions,
+            constraints: [],
+            validator: IsValidMetricConstraint,
+        });
+    };
+}
