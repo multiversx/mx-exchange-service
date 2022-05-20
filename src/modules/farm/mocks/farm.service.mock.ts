c3b1afc6888e4bd3372a464ff5872f676cb40d74
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-05-20 17:42:08 +0300

    SERVICES-83 add missing field for EsdtToken model
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/farm/mocks/farm.service.mock.ts
+++ src/modules/farm/mocks/farm.service.mock.ts
@@ -1,4 +1,8 @@
-import { EsdtToken } from 'src/modules/tokens/models/esdtToken.model';
+import {
+    AssetsModel,
+    EsdtToken,
+    RolesModel,
+} from 'src/modules/tokens/models/esdtToken.model';
 import { FarmTokenAttributesModel } from '../models/farmTokenAttributes.model';
 
 const farmMetadata = {
@@ -28,6 +32,7 @@ export class FarmServiceMock {
         return {
             identifier: 'LPT-1111',
             name: 'LiquidityPoolToken',
+            ticker: 'LPT',
             type: 'FungibleESDT',
             owner: 'user_address_1',
             supply: '0',
@@ -40,6 +45,23 @@ export class FarmServiceMock {
             canPause: true,
             canFreeze: true,
             canWipe: true,
+            minted: '1',
+            burnt: '1',
+            circulatingSupply: '1',
+            accounts: 1,
+            transactions: 1,
+            assets: new AssetsModel({
+                description: '',
+                extraTokens: [],
+                lockedAccounts: [],
+                pngUrl: '',
+                status: '',
+                svgUrl: '',
+                website: '',
+            }),
+            initialMinted: '1',
+            price: '1',
+            roles: new RolesModel(),
         };
     }
 
--- src/modules/tokens/models/esdtToken.model.ts
+++ src/modules/tokens/models/esdtToken.model.ts
@@ -1,12 +1,48 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class AssetsModel {
+    @Field({ nullable: true }) website: string;
+    @Field({ nullable: true }) description: string;
+    @Field({ nullable: true }) status: string;
+    @Field({ nullable: true }) pngUrl: string;
+    @Field({ nullable: true }) svgUrl: string;
+    @Field(() => [String], { nullable: true }) lockedAccounts: string[];
+    @Field(() => [String], { nullable: true }) extraTokens: string[];
+
+    constructor(init?: Partial<AssetsModel>) {
+        Object.assign(this, init);
+    }
+}
+
+@ObjectType()
+export class RolesModel {
+    @Field({ nullable: true }) address: string;
+    @Field({ nullable: true }) canMint: boolean;
+    @Field({ nullable: true }) canBurn: boolean;
+    @Field(() => [String], { nullable: true }) roles: string[];
+
+    constructor(init?: Partial<RolesModel>) {
+        Object.assign(this, init);
+    }
+}
 
 @ObjectType()
 export class EsdtToken {
     @Field() identifier: string;
     @Field() name: string;
+    @Field() ticker: string;
     @Field() owner: string;
-    @Field() supply: string;
+    @Field() minted: string;
+    @Field() burnt: string;
+    @Field() initialMinted: string;
     @Field() decimals: number;
+    @Field() price: string;
+    @Field() supply: string;
+    @Field() circulatingSupply: string;
+    @Field(() => AssetsModel, { nullable: true }) assets: AssetsModel;
+    @Field(() => Int) transactions: number;
+    @Field(() => Int) accounts: number;
     @Field() isPaused: boolean;
     @Field() canUpgrade: boolean;
     @Field() canMint: boolean;
@@ -15,6 +51,7 @@ export class EsdtToken {
     @Field() canPause: boolean;
     @Field() canFreeze: boolean;
     @Field() canWipe: boolean;
+    @Field(() => RolesModel, { nullable: true }) roles: RolesModel;
     @Field({ nullable: true }) type: string;
     @Field({ nullable: true }) balance?: string;
 
