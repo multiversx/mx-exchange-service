d61f79f897aa4db67269929ef479ca271fb98810
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-05-23 11:03:02 +0300

    SERVICES-83 [token] resolve token type field from mongodb
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/tokens/token.resolver.ts
+++ src/modules/tokens/token.resolver.ts
@@ -3,22 +3,37 @@ import { ApolloError } from 'apollo-server-express';
 import { PairGetterService } from '../pair/services/pair.getter.service';
 import { EsdtToken } from './models/esdtToken.model';
 import { TokensFiltersArgs } from './models/tokens.filter.args';
+import { TokenGetterService } from './services/token.getter.service';
 import { TokenService } from './services/token.service';
 
 @Resolver(() => EsdtToken)
 export class TokensResolver {
     constructor(
         private readonly tokenService: TokenService,
+        private readonly tokenGetter: TokenGetterService,
         private readonly pairGetter: PairGetterService,
     ) {}
 
-    @ResolveField()
-    async price(parent: EsdtToken): Promise<string> {
+    private async genericFieldResover(fieldResolver: () => any): Promise<any> {
         try {
-            return await this.pairGetter.getTokenPriceUSD(parent.identifier);
+            return await fieldResolver();
         } catch (error) {
             throw new ApolloError(error);
         }
+    }
+
+    @ResolveField()
+    async price(parent: EsdtToken): Promise<string> {
+        return await this.genericFieldResover(() =>
+            this.pairGetter.getTokenPriceUSD(parent.identifier),
+        );
+    }
+
+    @ResolveField()
+    async type(parent: EsdtToken): Promise<string> {
+        return await this.genericFieldResover(() =>
+            this.tokenGetter.getEsdtTokenType(parent.identifier),
+        );
     }
 
     @Query(() => [EsdtToken])
