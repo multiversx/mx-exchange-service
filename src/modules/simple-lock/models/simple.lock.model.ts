befe790d76909bdf3c52d06612a87073290d8247
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-05-27 11:42:56 +0300

    SERVICES-98 [simple-lock] use tokens attributes decoders from erdjs-dex
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/simple-lock/models/simple.lock.model.ts
+++ src/modules/simple-lock/models/simple.lock.model.ts
@@ -1,11 +1,4 @@
-import {
-    EnumType,
-    EnumVariantDefinition,
-    FieldDefinition,
-    StructType,
-    TokenIdentifierType,
-    U64Type,
-} from '@elrondnetwork/erdjs/out';
+import { EnumType, EnumVariantDefinition } from '@elrondnetwork/erdjs/out';
 import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { NftCollection } from 'src/modules/tokens/models/nftCollection.model';
 import { FarmTokenAttributesModel } from 'src/modules/farm/models/farmTokenAttributes.model';
@@ -38,28 +31,6 @@ export class LockedTokenAttributesModel {
     constructor(init?: Partial<LockedTokenAttributesModel>) {
         Object.assign(this, init);
     }
-
-    static getStructure(): StructType {
-        return new StructType('LockedTokenAttributes', [
-            new FieldDefinition(
-                'originalTokenID',
-                '',
-                new TokenIdentifierType(),
-            ),
-            new FieldDefinition('originalTokenNonce', '', new U64Type()),
-            new FieldDefinition('unlockEpoch', '', new U64Type()),
-        ]);
-    }
-
-    static fromDecodedAttributes(
-        decodedAttributes: any,
-    ): LockedTokenAttributesModel {
-        return new LockedTokenAttributesModel({
-            originalTokenID: decodedAttributes.originalTokenID.toString(),
-            originalTokenNonce: decodedAttributes.originalTokenNonce.toNumber(),
-            unlockEpoch: decodedAttributes.unlockEpoch.toNumber(),
-        });
-    }
 }
 
 @ObjectType()
@@ -81,28 +52,6 @@ export class LpProxyTokenAttributesModel {
 
     constructor(init?: Partial<LpProxyTokenAttributesModel>) {
         Object.assign(this, init);
-    }
-
-    static getStructure(): StructType {
-        return new StructType('LpProxyTokenAttributes', [
-            new FieldDefinition('lpTokenID', '', new TokenIdentifierType()),
-            new FieldDefinition('firstTokenID', '', new TokenIdentifierType()),
-            new FieldDefinition('firstTokenLockedNonce', '', new U64Type()),
-            new FieldDefinition('secondTokenID', '', new TokenIdentifierType()),
-            new FieldDefinition('secondTokenLockedNonce', '', new U64Type()),
-        ]);
-    }
-
-    static fromDecodedAttributes(
-        decodedAttributes: any,
-    ): LpProxyTokenAttributesModel {
-        return new LpProxyTokenAttributesModel({
-            lpTokenID: decodedAttributes.lpTokenID.toString(),
-            firstTokenID: decodedAttributes.firstTokenID.toString(),
-            firstTokenLockedNonce: decodedAttributes.firstTokenLockedNonce.toNumber(),
-            secondTokenID: decodedAttributes.secondTokenID.toString(),
-            secondTokenLockedNonce: decodedAttributes.secondTokenLockedNonce.toNumber(),
-        });
     }
 }
 
@@ -127,39 +76,6 @@ export class FarmProxyTokenAttributesModel {
 
     constructor(init?: Partial<FarmProxyTokenAttributesModel>) {
         Object.assign(this, init);
-    }
-
-    static getStructure(): StructType {
-        return new StructType('FarmProxyTokenAttributes', [
-            new FieldDefinition(
-                'farmType',
-                '',
-                new EnumType('FarmType', [
-                    new EnumVariantDefinition('SimpleFarm', 0),
-                    new EnumVariantDefinition('FarmWithLockedRewards', 1),
-                ]),
-            ),
-            new FieldDefinition('farmTokenID', '', new TokenIdentifierType()),
-            new FieldDefinition('farmTokenNonce', '', new U64Type()),
-            new FieldDefinition(
-                'farmingTokenID',
-                '',
-                new TokenIdentifierType(),
-            ),
-            new FieldDefinition('farmingTokenLockedNonce', '', new U64Type()),
-        ]);
-    }
-
-    static fromDecodedAttributes(
-        decodedAttributes: any,
-    ): FarmProxyTokenAttributesModel {
-        return new FarmProxyTokenAttributesModel({
-            farmType: decodedAttributes.farmType.name,
-            farmTokenID: decodedAttributes.farmTokenID.toString(),
-            farmTokenNonce: decodedAttributes.farmTokenNonce.toNumber(),
-            farmingTokenID: decodedAttributes.farmingTokenID.toString(),
-            farmingTokenLockedNonce: decodedAttributes.farmingTokenLockedNonce.toNumber(),
-        });
     }
 }
 
