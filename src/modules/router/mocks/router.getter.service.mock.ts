8ca41ae4828a0ce837cae87732d4729d0a47efe8
johnykes <johnslymusic@gmail.com>
2022-06-02 16:33:51 +0300

    SERVICES-129 update unit-tests

--- src/modules/router/mocks/router.getter.service.mock.ts
+++ src/modules/router/mocks/router.getter.service.mock.ts
@@ -1,27 +1,19 @@
 import { PairMetadata } from '../models/pair.metadata.model';
-
+import { pairs } from 'src/modules/pair/mocks/pair.constants';
 export class RouterGetterServiceMock {
     async getAllPairsAddress(): Promise<string[]> {
-        return [
-            'erd1qqqqqqqqqqqqqpgqe8m9w7cv2ekdc28q5ahku9x3hcregqpn0n4sum0e3u',
-            'erd1qqqqqqqqqqqqqpgqq67uv84ma3cekpa55l4l68ajzhq8qm3u0n4s20ecvx',
-        ];
+        return pairs.map(p => {
+            return p.address;
+        });
     }
 
     async getPairsMetadata(): Promise<PairMetadata[]> {
-        return [
-            new PairMetadata({
-                address:
-                    'erd1qqqqqqqqqqqqqpgqe8m9w7cv2ekdc28q5ahku9x3hcregqpn0n4sum0e3u',
-                firstTokenID: 'TOK1-1111',
-                secondTokenID: 'TOK2-2222',
-            }),
-            new PairMetadata({
-                address:
-                    'erd1qqqqqqqqqqqqqpgqq67uv84ma3cekpa55l4l68ajzhq8qm3u0n4s20ecvx',
-                firstTokenID: 'TOK1-1111',
-                secondTokenID: 'USDC-1111',
-            }),
-        ];
+        return pairs.map(p => {
+            return new PairMetadata({
+                firstTokenID: p.firstToken.identifier,
+                secondTokenID: p.secondToken.identifier,
+                address: p.address,
+            });
+        });
     }
 }
