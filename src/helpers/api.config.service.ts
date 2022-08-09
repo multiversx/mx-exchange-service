b629eed87cbb3d4498b0a57c3db423d19bc22219
johnykes <johnslymusic@gmail.com>
2022-08-09 10:39:26 +0300

    SERVICES-414 improve api.config.service.ts

--- src/helpers/api.config.service.ts
+++ src/helpers/api.config.service.ts
@@ -5,102 +5,56 @@ import { ConfigService } from '@nestjs/config';
 export class ApiConfigService {
     constructor(private readonly configService: ConfigService) {}
 
-    getPublicAppPort(): number {
-        const port = this.configService.get<number>('PORT');
-        if (!port) {
-            throw new Error('No public app port present');
+    private getGeneric<T>(path: string, alias?: string): T {
+        const value = this.configService.get<T>(path);
+        if (!value) {
+            throw new Error(`No '${alias || path}' config present`);
         }
-        return port;
+        return value;
+    }
+
+    getPublicAppPort(): number {
+        return parseInt(this.getGeneric<string>('PORT'));
     }
 
     getPublicAppListenAddress(): string {
-        const listenAddress = this.configService.get<string>('LISTEN_ADDRESS');
-        if (!listenAddress) {
-            throw new Error('No public app listen address present');
-        }
-        return listenAddress;
+        return this.getGeneric<string>('LISTEN_ADDRESS');
     }
 
     getPrivateAppPort(): number {
-        const port = this.configService.get<number>('PRIVATE_PORT');
-        if (!port) {
-            throw new Error('No private app port present');
-        }
-        return port;
+        return parseInt(this.getGeneric<string>('PRIVATE_PORT'));
     }
 
     getPrivateAppListenAddress(): string {
-        const listenAddress = this.configService.get<string>(
-            'PRIVATE_LISTEN_ADDRESS',
-        );
-        if (!listenAddress) {
-            throw new Error('No private app listen address present');
-        }
-        return listenAddress;
+        return this.getGeneric<string>('PRIVATE_LISTEN_ADDRESS');
     }
 
     getCacheWarmerPort(): number {
-        const port = this.configService.get<number>('CACHEWARMER_PORT');
-        if (!port) {
-            throw new Error('No cache warmer app port present');
-        }
-        return port;
+        return parseInt(this.getGeneric<string>('CACHEWARMER_PORT'));
     }
 
     isPublicApiActive(): boolean {
-        const publicApiActive = this.configService.get<string>(
-            'ENABLE_PUBLIC_API',
-        );
-        if (!publicApiActive) {
-            throw new Error('No public api flag present');
-        }
-        return publicApiActive === 'true';
+        return this.getGeneric<string>('ENABLE_PUBLIC_API') === 'true';
     }
 
     isCacheWarmerCronActive(): boolean {
-        const cacheWramerActive = this.configService.get<string>(
-            'ENABLE_CACHE_WARMER',
-        );
-        if (!cacheWramerActive) {
-            throw new Error('No cache warmer flag present');
-        }
-        return cacheWramerActive === 'true';
+        return this.getGeneric<string>('ENABLE_CACHE_WARMER') === 'true';
     }
 
     isPrivateAppActive(): boolean {
-        const privateAppActive = this.configService.get<string>(
-            'ENABLE_PRIVATE_API',
-        );
-        if (!privateAppActive) {
-            throw new Error('No private api flag present');
-        }
-        return privateAppActive === 'true';
+        return this.getGeneric<string>('ENABLE_PRIVATE_API') === 'true';
     }
 
     isEventsNotifierAppActive(): boolean {
-        const eventsNotifierAppActive = this.configService.get<string>(
-            'ENABLE_EVENTS_NOTIFIER',
-        );
-        if (!eventsNotifierAppActive) {
-            throw new Error('No events notifier api flag present');
-        }
-        return eventsNotifierAppActive === 'true';
+        return this.getGeneric<string>('ENABLE_EVENTS_NOTIFIER') === 'true';
     }
 
     getRedisUrl(): string {
-        const redisUrl = this.configService.get<string>('REDIS_URL');
-        if (!redisUrl) {
-            throw new Error('No redis url present');
-        }
-        return redisUrl;
+        return this.getGeneric<string>('REDIS_URL');
     }
 
     getRedisPort(): number {
-        const redisPort = this.configService.get<number>('REDIS_PORT');
-        if (!redisPort) {
-            throw new Error('No redis port present');
-        }
-        return redisPort;
+        return parseInt(this.getGeneric<string>('REDIS_PORT'));
     }
 
     getRedisPassword(): string | undefined {
@@ -109,86 +63,40 @@ export class ApiConfigService {
     }
 
     getApiUrl(): string {
-        const apiUrl = this.configService.get<string>('ELRONDAPI_URL');
-        if (!apiUrl) {
-            throw new Error('No apiUrl present');
-        }
-        return apiUrl;
+        return this.getGeneric<string>('ELRONDAPI_URL');
     }
 
     getNotifierUrl(): string {
-        const notifierUrl = this.configService.get<string>('NOTIFIER_URL');
-        if (!notifierUrl) {
-            throw new Error('No notifier url present');
-        }
-        return notifierUrl;
+        return this.getGeneric<string>('NOTIFIER_URL');
     }
 
     getKeepAliveTimeoutDownstream(): number {
-        const keepAliveTimeoutDownstream = this.configService.get<string>(
-            'KEEPALIVE_TIMEOUT_DOWNSTREAM',
+        return parseInt(
+            this.getGeneric<string>('KEEPALIVE_TIMEOUT_DOWNSTREAM'),
         );
-        if (!keepAliveTimeoutDownstream) {
-            throw new Error('No keepAliveTimeoutDownstream present');
-        }
-
-        return parseInt(keepAliveTimeoutDownstream);
     }
 
     getKeepAliveTimeoutUpstream(): number {
-        const keepAliveTimeoutUpstream = this.configService.get<string>(
-            'KEEPALIVE_TIMEOUT_UPSTREAM',
-        );
-        if (!keepAliveTimeoutUpstream) {
-            throw new Error('No keepAliveTimeoutUpstream present');
-        }
-        return parseInt(keepAliveTimeoutUpstream);
+        return parseInt(this.getGeneric<string>('KEEPALIVE_TIMEOUT_UPSTREAM'));
     }
 
     getMongoDBURL(): string {
-        const mongoDBUrl = this.configService.get<string>('MONGODB_URL');
-        if (!mongoDBUrl) {
-            throw new Error('No MongoDB URL present');
-        }
-        return mongoDBUrl;
+        return this.getGeneric<string>('MONGODB_URL');
     }
 
     getMongoDBDatabase(): string {
-        const mongoDBDatabase = this.configService.get<string>(
-            'MONGODB_DATABASE',
-        );
-        if (!mongoDBDatabase) {
-            throw new Error('No MongoDB Database present');
-        }
-        return mongoDBDatabase;
+        return this.getGeneric<string>('MONGODB_DATABASE');
     }
 
     getMongoDBUsername(): string {
-        const mongoDBUsername = this.configService.get<string>(
-            'MONGODB_USERNAME',
-        );
-        if (!mongoDBUsername) {
-            throw new Error('No MongoDB username present');
-        }
-        return mongoDBUsername;
+        return this.getGeneric<string>('MONGODB_USERNAME');
     }
 
     getMongoDBPassword(): string {
-        const mongoDBPassword = this.configService.get<string>(
-            'MONGODB_PASSWORD',
-        );
-        if (!mongoDBPassword) {
-            throw new Error('No MongoDB password present');
-        }
-        return mongoDBPassword;
+        return this.getGeneric<string>('MONGODB_PASSWORD');
     }
 
     getSecurityAdmins(): string[] {
-        const admins = this.configService.get<string>('SECURITY_ADMINS');
-        if (!admins) {
-            throw new Error('No security admins present');
-        }
-
-        return admins.split(',');
+        return this.getGeneric<string[]>('SECURITY_ADMINS');
     }
 }
