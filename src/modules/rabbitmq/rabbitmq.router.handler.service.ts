65535d1f46d8c87be5c6835a528371f3c933b935
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-08-25 15:30:14 +0300

    SERVICES-532 invalidate all tokens paths on create pair event
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/rabbitmq/rabbitmq.router.handler.service.ts
+++ src/modules/rabbitmq/rabbitmq.router.handler.service.ts
@@ -4,20 +4,22 @@ import { RedisPubSub } from 'graphql-redis-subscriptions';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { constantsConfig } from 'src/config';
 import { PUB_SUB } from 'src/services/redis.pubSub.module';
+import { generateCacheKeyFromParams } from 'src/utils/generate-cache-key';
 import { Logger } from 'winston';
 import { AbiRouterService } from '../router/services/abi.router.service';
 import { RouterSetterService } from '../router/services/router.setter.service';
 import { CreateTokenDto } from '../tokens/dto/create.token.dto';
 import { TokenGetterService } from '../tokens/services/token.getter.service';
 import { TokenRepositoryService } from '../tokens/services/token.repository.service';
+import { TokenService } from '../tokens/services/token.service';
 
 @Injectable()
 export class RabbitMQRouterHandlerService {
-    private invalidatedKeys = [];
     constructor(
         private readonly routerAbiService: AbiRouterService,
         private readonly routerSetterService: RouterSetterService,
         private readonly tokenGetter: TokenGetterService,
+        private readonly tokenService: TokenService,
         private readonly tokenRepository: TokenRepositoryService,
         @Inject(PUB_SUB) private pubSub: RedisPubSub,
         @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
@@ -33,11 +35,13 @@ export class RabbitMQRouterHandlerService {
             pairsAddresses,
             firstTokenType,
             secondTokenType,
+            uniqueTokens,
         ] = await Promise.all([
             this.routerAbiService.getPairsMetadata(),
             this.routerAbiService.getAllPairsAddress(),
             this.tokenGetter.getEsdtTokenType(firstTokenID),
             this.tokenGetter.getEsdtTokenType(secondTokenID),
+            this.tokenService.getUniqueTokenIDs(true),
         ]);
 
         if (
@@ -65,16 +69,42 @@ export class RabbitMQRouterHandlerService {
             this.routerSetterService.setPairsMetadata(pairsMetadata),
             this.routerSetterService.setAllPairsAddress(pairsAddresses),
         ]);
-        this.invalidatedKeys.push(...keys);
-        await this.deleteCacheKeys();
+
+        for (const token of uniqueTokens) {
+            keys.push(
+                ...[
+                    generateCacheKeyFromParams(
+                        'auto.route.paths',
+                        firstTokenID,
+                        token,
+                    ),
+                    generateCacheKeyFromParams(
+                        'auto.route.paths',
+                        secondTokenID,
+                        token,
+                    ),
+                    generateCacheKeyFromParams(
+                        'auto.route.paths',
+                        token,
+                        firstTokenID,
+                    ),
+                    generateCacheKeyFromParams(
+                        'auto.route.paths',
+                        token,
+                        secondTokenID,
+                    ),
+                ],
+            );
+        }
+
+        await this.deleteCacheKeys(keys);
 
         await this.pubSub.publish(ROUTER_EVENTS.CREATE_PAIR, {
             createPairEvent: event,
         });
     }
 
-    private async deleteCacheKeys() {
-        await this.pubSub.publish('deleteCacheKeys', this.invalidatedKeys);
-        this.invalidatedKeys = [];
+    private async deleteCacheKeys(invalidatedKeys: string[]) {
+        await this.pubSub.publish('deleteCacheKeys', invalidatedKeys);
     }
 }
