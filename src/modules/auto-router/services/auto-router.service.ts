d37fadf295084b69ae0b56c5ef697b0c3ea58325
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-08-25 10:58:27 +0300

    SERVICES-438 throw error on swap request for multi swap spread impact
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/auto-router/services/auto-router.service.ts
+++ src/modules/auto-router/services/auto-router.service.ts
@@ -243,6 +243,13 @@ export class AutoRouterService {
             swapRoute.intermediaryAmounts,
         );
 
+        if (
+            pairs.length > 1 &&
+            priceDeviationPercent > constantsConfig.MAX_SWAP_SPREAD
+        ) {
+            throw new Error('Spread too big!');
+        }
+
         return new AutoRouteModel({
             swapType: swapType,
             tokenInID: args.tokenInID,
