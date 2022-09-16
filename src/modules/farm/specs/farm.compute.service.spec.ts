de478354a6a43fe3bb5aaea2be851cf469ac6d08
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-09-16 19:28:06 +0300

    SERVICES-68 fix farm unit tests
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/farm/specs/farm.compute.service.spec.ts
+++ src/modules/farm/specs/farm.compute.service.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ContextService } from '../../../services/context/context.service';
 import { PairService } from '../../pair/services/pair.service';
 import { FarmService } from '../services/farm.service';
 import { AbiFarmService } from '../services/abi-farm.service';
@@ -7,7 +6,6 @@ import { AbiFarmServiceMock } from '../mocks/abi.farm.service.mock';
 import { ElrondApiService } from '../../../services/elrond-communication/elrond-api.service';
 import { ElrondApiServiceMock } from '../../../services/elrond-communication/elrond.api.service.mock';
 import { FarmTokenAttributesModel } from '../models/farmTokenAttributes.model';
-import { ContextServiceMock } from '../../../services/context/mocks/context.service.mock';
 import { CommonAppModule } from '../../../common.app.module';
 import { CachingModule } from '../../../services/caching/cache.module';
 import { FarmGetterService } from '../services/farm.getter.service';
@@ -16,12 +14,13 @@ import { FarmGetterServiceMock } from '../mocks/farm.getter.service.mock';
 import { PairGetterService } from '../../../modules/pair/services/pair.getter.service';
 import { PairGetterServiceMock } from '../../../modules/pair/mocks/pair.getter.service.mock';
 import { PairComputeService } from '../../../modules/pair/services/pair.compute.service';
-import { PriceFeedService } from '../../../services/price-feed/price-feed.service';
-import { PriceFeedServiceMock } from '../../../services/price-feed/price.feed.service.mock';
 import { ContextGetterService } from '../../../services/context/context.getter.service';
 import { ContextGetterServiceMock } from '../../../services/context/mocks/context.getter.service.mock';
 import { WrapService } from '../../wrapping/wrap.service';
 import { WrapServiceMock } from '../../wrapping/wrap.test-mocks';
+import { TokenComputeService } from 'src/modules/tokens/services/token.compute.service';
+import { RouterGetterServiceProvider } from 'src/modules/router/mocks/router.getter.service.mock';
+import { TokenGetterServiceProvider } from 'src/modules/tokens/mocks/token.getter.service.mock';
 
 describe('FarmService', () => {
     let service: FarmComputeService;
@@ -41,11 +40,6 @@ describe('FarmService', () => {
         useClass: ElrondApiServiceMock,
     };
 
-    const ContextServiceProvider = {
-        provide: ContextService,
-        useClass: ContextServiceMock,
-    };
-
     const ContextGetterServiceProvider = {
         provide: ContextGetterService,
         useClass: ContextGetterServiceMock,
@@ -54,11 +48,6 @@ describe('FarmService', () => {
     const PairGetterServiceProvider = {
         provide: PairGetterService,
         useClass: PairGetterServiceMock,
-    };
-
-    const PriceFeedServiceProvider = {
-        provide: PriceFeedService,
-        useClass: PriceFeedServiceMock,
     };
 
     const WrapServiceProvider = {
@@ -74,12 +63,13 @@ describe('FarmService', () => {
                 FarmGetterServiceProvider,
                 FarmComputeService,
                 ElrondApiServiceProvider,
-                ContextServiceProvider,
                 ContextGetterServiceProvider,
                 PairService,
                 PairGetterServiceProvider,
                 PairComputeService,
-                PriceFeedServiceProvider,
+                TokenGetterServiceProvider,
+                TokenComputeService,
+                RouterGetterServiceProvider,
                 WrapServiceProvider,
                 FarmService,
             ],
@@ -96,21 +86,21 @@ describe('FarmService', () => {
         const farmAPR = await service.computeUnlockedRewardsAPR(
             'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
         );
-        expect(farmAPR).toEqual('10.00043799890500273749');
+        expect(farmAPR).toEqual('10.00004379989050027374');
     });
 
     it('should compute farmed token price USD', async () => {
         const farmedTokenPriceUSD = await service.computeFarmedTokenPriceUSD(
             'farm_address_2',
         );
-        expect(farmedTokenPriceUSD).toEqual('100');
+        expect(farmedTokenPriceUSD).toEqual('10');
     });
 
     it('should compute farming token price USD', async () => {
         const farmingTokenPriceUSD = await service.computeFarmingTokenPriceUSD(
             'farm_address_2',
         );
-        expect(farmingTokenPriceUSD).toEqual('400');
+        expect(farmingTokenPriceUSD).toEqual('40');
     });
 
     it('should compute farm locked value USD', async () => {
@@ -121,57 +111,65 @@ describe('FarmService', () => {
     });
 
     it('should compute farm rewards for position', async () => {
-        const farmRewardsForPosition = await service.computeFarmRewardsForPosition(
-            'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
-            '100000000000000000000000000000',
-            new FarmTokenAttributesModel({
-                identifier: undefined,
-                attributes: undefined,
-                rewardPerShare: '100',
-                originalEnteringEpoch: 0,
-                enteringEpoch: 0,
-                aprMultiplier: 25,
-                initialFarmingAmount: '10000000000000000000',
-                compoundedReward: '500000000000000',
-                currentFarmAmount: '100000000000000',
-                lockedRewards: true,
-            }),
+        const farmRewardsForPosition =
+            await service.computeFarmRewardsForPosition(
+                'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
+                '100000000000000000000000000000',
+                new FarmTokenAttributesModel({
+                    identifier: undefined,
+                    attributes: undefined,
+                    rewardPerShare: '100',
+                    originalEnteringEpoch: 0,
+                    enteringEpoch: 0,
+                    aprMultiplier: 25,
+                    initialFarmingAmount: '10000000000000000000',
+                    compoundedReward: '500000000000000',
+                    currentFarmAmount: '100000000000000',
+                    lockedRewards: true,
+                }),
+            );
+        expect(farmRewardsForPosition.toFixed()).toEqual(
+            '18333333333333333350000000',
         );
-        expect(farmRewardsForPosition.toFixed()).toEqual('18333333333333333350000000');
     });
 
     it('should compute locked farming token reserve', async () => {
-        const lockedFarmingTokenReserve = await service.computeLockedFarmingTokenReserve(
-            'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
-        );
+        const lockedFarmingTokenReserve =
+            await service.computeLockedFarmingTokenReserve(
+                'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
+            );
         expect(lockedFarmingTokenReserve).toEqual('200000000000000000000000');
     });
 
     it('should compute unlocked farming token reserve', async () => {
-        const unlockedFarmingTokenReserve = await service.computeUnlockedFarmingTokenReserve(
-            'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
-        );
+        const unlockedFarmingTokenReserve =
+            await service.computeUnlockedFarmingTokenReserve(
+                'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
+            );
         expect(unlockedFarmingTokenReserve).toEqual('200000000000000000000000');
     });
 
     it('should compute locked farming token reserve USD', async () => {
-        const lockedFarmingTokenReserveUSD = await service.computeLockedFarmingTokenReserveUSD(
-            'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
-        );
+        const lockedFarmingTokenReserveUSD =
+            await service.computeLockedFarmingTokenReserveUSD(
+                'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
+            );
         expect(lockedFarmingTokenReserveUSD).toEqual('16000040000000');
     });
 
     it('should compute unlocked farming token reserve USD', async () => {
-        const unlockedFarmingTokenReserveUSD = await service.computeUnlockedFarmingTokenReserveUSD(
-            'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
-        );
+        const unlockedFarmingTokenReserveUSD =
+            await service.computeUnlockedFarmingTokenReserveUSD(
+                'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
+            );
         expect(unlockedFarmingTokenReserveUSD).toEqual('16000040000000');
     });
 
     it('should compute virtual value locked USD', async () => {
-        const virtualValueLockedUSD = await service.computeVirtualValueLockedUSD(
-            'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
-        );
+        const virtualValueLockedUSD =
+            await service.computeVirtualValueLockedUSD(
+                'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
+            );
         expect(virtualValueLockedUSD).toEqual('48000120000000');
     });
 
@@ -179,21 +177,21 @@ describe('FarmService', () => {
         const anualRewardsUSD = await service.computeAnualRewardsUSD(
             'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
         );
-        expect(anualRewardsUSD).toEqual('21024000000');
+        expect(anualRewardsUSD).toEqual('2102400000');
     });
 
     it('should compute unlocked rewards APR', async () => {
         const unlockedRewardsAPR = await service.computeUnlockedRewardsAPR(
             'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
         );
-        expect(unlockedRewardsAPR).toEqual('10.00043799890500273749');
+        expect(unlockedRewardsAPR).toEqual('10.00004379989050027374');
     });
 
     it('should compute locked rewards APR', async () => {
         const lockedRewardsAPR = await service.computeLockedRewardsAPR(
             'erd18h5dulxp5zdp80qjndd2w25kufx0rm5yqd2h7ajrfucjhr82y8vqyq0hye',
         );
-        expect(lockedRewardsAPR).toEqual('10.00087599781000547498');
+        expect(lockedRewardsAPR).toEqual('10.00008759978100054749');
     });
 
     it('should compute farm APR', async () => {
@@ -203,6 +201,6 @@ describe('FarmService', () => {
         expect(farmAPR_0).toEqual(null);
 
         const farmAPR_1 = await service.computeFarmAPR('farm_address_2');
-        expect(farmAPR_1).toEqual('10.5256');
+        expect(farmAPR_1).toEqual('10.05256');
     });
 });
