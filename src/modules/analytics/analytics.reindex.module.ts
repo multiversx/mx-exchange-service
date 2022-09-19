3066e5046440da520173894c332f7cd7476b7bae
johnykes <johnslymusic@gmail.com>
2022-09-19 10:25:31 +0300

    SERVICES-414 remove comments & unused imports

--- src/modules/analytics/analytics.reindex.module.ts
+++ src/modules/analytics/analytics.reindex.module.ts
@@ -3,20 +3,12 @@ import { AWSModule } from 'src/services/aws/aws.module';
 import { CachingModule } from 'src/services/caching/cache.module';
 import { ContextModule } from 'src/services/context/context.module';
 import { ElrondCommunicationModule } from 'src/services/elrond-communication/elrond-communication.module';
-import { FarmModule } from '../farm/farm.module';
 import { PairModule } from '../pair/pair.module';
 import { RouterModule } from '../router/router.module';
-import { PriceDiscoveryModule } from '../price-discovery/price.discovery.module';
 import { ElasticService } from 'src/helpers/elastic.service';
 import { LogsProcessorService } from 'src/services/crons/logs.processor.service';
 import { AnalyticsReindexService } from './services/analytics.reindex.service';
 import { SchedulerRegistry } from '@nestjs/schedule';
-import { ProxyModule } from '../proxy/proxy.module';
-import { AnalyticsComputeService } from './services/analytics.compute.service';
-import { AnalyticsGetterService } from './services/analytics.getter.service';
-import { AnalyticsAWSGetterService } from './services/analytics.service';
-import { AnalyticsEventHandlerService } from './services/analytics.event.handler.service';
-import { AnalyticsPairService } from './services/analytics.pair.service';
 
 @Module({
     imports: [
@@ -26,7 +18,6 @@ import { AnalyticsPairService } from './services/analytics.pair.service';
         ContextModule,
         RouterModule,
         PairModule,
-        //PriceDiscoveryModule,
     ],
     providers: [
         AnalyticsReindexService,
