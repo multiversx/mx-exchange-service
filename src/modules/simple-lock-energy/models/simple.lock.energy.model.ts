a0e0b6c4b069b4247ad5e44b32f7ce3c692968c8
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-09-19 14:31:05 +0300

    SERVICES-626 add Energy struct model
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/simple-lock-energy/models/simple.lock.energy.model.ts
+++ src/modules/simple-lock-energy/models/simple.lock.energy.model.ts
@@ -1,3 +1,9 @@
+import {
+    BigUIntType,
+    FieldDefinition,
+    StructType,
+    U64Type,
+} from '@elrondnetwork/erdjs/out';
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
@@ -10,4 +16,26 @@ export class SimpleLockEnergyModel {
     baseAssetTokenID: string;
     @Field(() => [Int])
     lockOptions: number[];
+}
+
+@ObjectType()
+export class Energy {
+    @Field()
+    amount: string;
+    @Field(() => Int)
+    lastUpdateEpoch: number;
+    @Field()
+    totalLockedTokens: string;
+
+    constructor(init?: Partial<Energy>) {
+        Object.assign(this, init);
+    }
+
+    static getStructure(): StructType {
+        return new StructType('Energy', [
+            new FieldDefinition('amount', '', new BigUIntType()),
+            new FieldDefinition('lastUpdateEpoch', '', new U64Type()),
+            new FieldDefinition('totalLockedTokens', '', new BigUIntType()),
+        ]);
+    }
 }
