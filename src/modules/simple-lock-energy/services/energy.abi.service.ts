a5f5ba84570e7b28afc16348194584f5e450850d
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-09-19 16:47:05 +0300

    SERVICES-626 add simple lock energy abi service
    - implement getters endpoints from contract
    
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/modules/simple-lock-energy/services/energy.abi.service.ts
@@ -0,0 +1,120 @@
+import {
+    Address,
+    AddressValue,
+    BigUIntValue,
+    Interaction,
+    U64Value,
+} from '@elrondnetwork/erdjs/out';
+import { Inject, Injectable } from '@nestjs/common';
+import BigNumber from 'bignumber.js';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { ElrondProxyService } from 'src/services/elrond-communication/elrond-proxy.service';
+import { GenericAbiService } from 'src/services/generics/generic.abi.service';
+import { Logger } from 'winston';
+import { Energy } from '../models/simple.lock.energy.model';
+
+@Injectable()
+export class EnergyAbiService extends GenericAbiService {
+    constructor(
+        protected readonly elrondProxy: ElrondProxyService,
+        @Inject(WINSTON_MODULE_PROVIDER) protected readonly logger: Logger,
+    ) {
+        super(elrondProxy, logger);
+    }
+
+    async getLockedTokenID(): Promise<string> {
+        const contract =
+            await this.elrondProxy.getSimpleLockEnergySmartContract();
+        const interaction: Interaction =
+            contract.methodsExplicit.getLockedTokenId();
+
+        const response = await this.getGenericData(
+            EnergyAbiService.name,
+            interaction,
+        );
+        return response.firstValue.valueOf().toString();
+    }
+
+    async getBaseAssetTokenID(): Promise<string> {
+        const contract =
+            await this.elrondProxy.getSimpleLockEnergySmartContract();
+        const interaction: Interaction =
+            contract.methodsExplicit.getBaseAssetTokenId();
+
+        const response = await this.getGenericData(
+            EnergyAbiService.name,
+            interaction,
+        );
+        return response.firstValue.valueOf().toString();
+    }
+
+    async getLockOptions(): Promise<number[]> {
+        const contract =
+            await this.elrondProxy.getSimpleLockEnergySmartContract();
+        const interaction: Interaction =
+            contract.methodsExplicit.getLockOptions();
+
+        const response = await this.getGenericData(
+            EnergyAbiService.name,
+            interaction,
+        );
+        return response.firstValue
+            .valueOf()
+            .map((lockOption: BigNumber) => lockOption.toNumber());
+    }
+
+    async getEnergyEntryForUser(userAddress: string): Promise<Energy> {
+        const contract =
+            await this.elrondProxy.getSimpleLockEnergySmartContract();
+        const interaction: Interaction =
+            contract.methodsExplicit.getEnergyEntryForUser([
+                new AddressValue(Address.fromString(userAddress)),
+            ]);
+
+        const response = await this.getGenericData(
+            EnergyAbiService.name,
+            interaction,
+        );
+        const rawEnergy = response.firstValue.valueOf();
+        return new Energy({
+            amount: rawEnergy.amount.toFixed(),
+            lastUpdateEpoch: rawEnergy.last_update_epoch.toNumber(),
+            totalLockedTokens: rawEnergy.total_locked_tokens.toFixed(),
+        });
+    }
+
+    async getEnergyAmountForUser(userAddress: string): Promise<string> {
+        const contract =
+            await this.elrondProxy.getSimpleLockEnergySmartContract();
+        const interaction: Interaction =
+            contract.methodsExplicit.getEnergyAmountForUser([
+                new AddressValue(Address.fromString(userAddress)),
+            ]);
+
+        const response = await this.getGenericData(
+            EnergyAbiService.name,
+            interaction,
+        );
+        return response.firstValue.valueOf().toFixed();
+    }
+
+    async getPenaltyAmount(
+        tokenAmount: string,
+        epochsToReduce: number,
+    ): Promise<string> {
+        const contract =
+            await this.elrondProxy.getSimpleLockEnergySmartContract();
+        const interaction: Interaction =
+            contract.methodsExplicit.getPenaltyAmount([
+                new BigUIntValue(new BigNumber(tokenAmount)),
+                new U64Value(new BigNumber(epochsToReduce)),
+            ]);
+
+        const response = await this.getGenericData(
+            EnergyAbiService.name,
+            interaction,
+        );
+
+        return response.firstValue.valueOf().toFixed();
+    }
+}
