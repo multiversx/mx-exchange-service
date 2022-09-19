fe11a2b133f285aebd7bbb2cc0b7a845a3e9de12
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-09-19 17:04:34 +0300

    SERVICES-626 add generic resolver class
    - implement generic field resolver
    - implement generic query for resolver
    
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/services/generics/generic.resolver.ts
@@ -0,0 +1,23 @@
+import { ApolloError } from 'apollo-server-express';
+
+export class GenericResolver {
+    protected async genericFieldResover<T>(
+        fieldResolver: () => Promise<T>,
+    ): Promise<T> {
+        try {
+            return await fieldResolver();
+        } catch (error) {
+            throw new ApolloError(error);
+        }
+    }
+
+    protected async genericQuery<T>(
+        queryResolver: () => Promise<T>,
+    ): Promise<T> {
+        try {
+            return await queryResolver();
+        } catch (error) {
+            throw new ApolloError(error);
+        }
+    }
+}
