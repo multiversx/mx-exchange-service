683c92da04f6f6021b216e3d1982da8b4bce5f7e
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-10-06 12:11:03 +0300

    fixes after self-review

--- src/modules/fees-collector/fees-collector.resolver.ts
+++ src/modules/fees-collector/fees-collector.resolver.ts
@@ -1,11 +1,11 @@
 import { Resolver } from '@nestjs/graphql';
 
 import { FeesCollectorModel } from "./models/fees-collector.model";
-import { FeesCollectorAbiService } from "./services/fees-collector.abi.service";
+import { FeesCollectorGetterService } from "./services/fees-collector.getter.service";
 
 @Resolver(() => FeesCollectorModel)
 export class FeesCollectorResolver {
     constructor(
-        private readonly farmGetterService: FeesCollectorAbiService // TODO: replace with getter service
+        private readonly farmGetterService: FeesCollectorGetterService
     ) {}
 }
