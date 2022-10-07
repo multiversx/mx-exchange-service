58aeb8ff3ea3892c22aad66b205c092258539cd3
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-10-07 14:15:33 +0300

    SERVICES-704 add resolver for farm v2 model
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/modules/farm/resolvers/farm.v2.resolver.ts
@@ -0,0 +1,34 @@
+import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { FarmModelV2 } from '../models/farm.v2.model';
+import { FarmV2GetterService } from '../services/v2/farm.v2.getter.service';
+import { FarmResolver } from './farm.resolver';
+
+@Resolver(() => FarmModelV2)
+export class FarmV2Resolver extends FarmResolver {
+    constructor(protected readonly farmGetter: FarmV2GetterService) {
+        super(farmGetter);
+    }
+
+    @ResolveField()
+    async boostedYieldsRewardsPercenatage(
+        @Parent() parent: FarmModelV2,
+    ): Promise<number> {
+        return await this.genericFieldResover(() =>
+            this.farmGetter.getBoostedYieldsRewardsPercenatage(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async currentWeek(@Parent() parent: FarmModelV2): Promise<number> {
+        return await this.genericFieldResover(() =>
+            this.farmGetter.getCurrentWeek(parent.address),
+        );
+    }
+
+    @ResolveField()
+    async energyFactoryAddress(@Parent() parent: FarmModelV2): Promise<string> {
+        return await this.genericFieldResover(() =>
+            this.farmGetter.getEnergyFactoryAddress(parent.address),
+        );
+    }
+}
