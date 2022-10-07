d351c1a88d0225b70eb7e4beedb3cc58f88d4458
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-10-07 17:33:59 +0300

    SERVICES-704 add farm v2 into farm query resolver
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/farm/resolvers/farm.query.resolver.ts
+++ src/modules/farm/resolvers/farm.query.resolver.ts
@@ -1,32 +1,27 @@
 import { UseGuards } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { ApolloError } from 'apollo-server-express';
-import { User } from 'src/helpers/userDecorator';
-import { TransactionModel } from 'src/models/transaction.model';
-import { GqlAdminGuard } from 'src/modules/auth/gql.admin.guard';
 import { GqlAuthGuard } from 'src/modules/auth/gql.auth.guard';
 import { GenericResolver } from 'src/services/generics/generic.resolver';
+import { farmVersion } from 'src/utils/farm.utils';
 import {
     BatchFarmRewardsComputeArgs,
     CalculateRewardsArgs,
-    ClaimRewardsArgs,
-    CompoundRewardsArgs,
-    EnterFarmArgs,
-    ExitFarmArgs,
-    FarmMigrationConfigArgs,
-    MergeFarmTokensArgs,
 } from '../models/farm.args';
-import { ExitFarmTokensModel, RewardsModel } from '../models/farm.model';
+import {
+    ExitFarmTokensModel,
+    FarmVersion,
+    RewardsModel,
+} from '../models/farm.model';
 import { FarmsUnion } from '../models/farm.union';
 import { FarmTokenAttributesModel } from '../models/farmTokenAttributes.model';
 import { FarmService } from '../services/farm.service';
-import { TransactionsFarmService } from '../services/farm.transaction.service';
+import { FarmV2Service } from '../services/v2/farm.v2.service';
 
 @Resolver()
 export class FarmQueryResolver extends GenericResolver {
     constructor(
         private readonly farmService: FarmService,
-        private readonly transactionsService: TransactionsFarmService,
+        private readonly farmV2Service: FarmV2Service,
     ) {
         super();
     }
@@ -43,7 +38,7 @@ export class FarmQueryResolver extends GenericResolver {
         @Args('identifier') identifier: string,
         @Args('attributes') attributes: string,
     ): Promise<FarmTokenAttributesModel> {
-        return this.farmService.decodeFarmTokenAttributes(
+        return this.getService(farmAddress).decodeFarmTokenAttributes(
             farmAddress,
             identifier,
             attributes,
@@ -56,7 +51,9 @@ export class FarmQueryResolver extends GenericResolver {
         @Args('farmsPositions') args: BatchFarmRewardsComputeArgs,
     ): Promise<RewardsModel[]> {
         return await this.genericQuery(() =>
-            this.farmService.getBatchRewardsForPosition(args.farmsPositions),
+            this.getService(
+                args.farmsPositions[0].farmAddress,
+            ).getBatchRewardsForPosition(args.farmsPositions),
         );
     }
 
@@ -66,7 +63,17 @@ export class FarmQueryResolver extends GenericResolver {
         @Args('args') args: CalculateRewardsArgs,
     ): Promise<ExitFarmTokensModel> {
         return await this.genericQuery(() =>
-            this.farmService.getTokensForExitFarm(args),
+            this.getService(args.farmAddress).getTokensForExitFarm(args),
         );
+    }
+
+    private getService(farmAddress: string): FarmService | FarmV2Service {
+        const version = farmVersion(farmAddress);
+        switch (version) {
+            case FarmVersion.V2:
+                return this.farmV2Service;
+            default:
+                return this.farmService;
+        }
     }
 }
--- src/utils/farm.utils.ts
+++ src/utils/farm.utils.ts
@@ -10,6 +10,8 @@ const toVersionEnum = (version: string): FarmVersion => {
             return FarmVersion.V1_2;
         case 'v1.3':
             return FarmVersion.V1_3;
+        case 'v2':
+            return FarmVersion.V2;
     }
 };
 
@@ -29,7 +31,7 @@ export const farmVersion = (farmAddress: string): FarmVersion | undefined => {
     for (const version of versions) {
         if (Array.isArray(farmsConfig[version])) {
             const address = farmsConfig[version].find(
-                address => address === farmAddress,
+                (address: string) => address === farmAddress,
             );
             if (address !== undefined) {
                 return toVersionEnum(version);
@@ -38,7 +40,7 @@ export const farmVersion = (farmAddress: string): FarmVersion | undefined => {
             const types = Object.keys(farmsConfig[version]);
             for (const type of types) {
                 const address = farmsConfig[version][type].find(
-                    address => address === farmAddress,
+                    (address: string) => address === farmAddress,
                 );
                 if (address !== undefined) {
                     return toVersionEnum(version);
@@ -54,7 +56,7 @@ export const farmType = (farmAddress: string): FarmRewardType | undefined => {
     for (const version of versions) {
         if (Array.isArray(farmsConfig[version])) {
             const address = farmsConfig[version].find(
-                address => address === farmAddress,
+                (address: string) => address === farmAddress,
             );
             if (address !== undefined) {
                 return undefined;
@@ -63,7 +65,7 @@ export const farmType = (farmAddress: string): FarmRewardType | undefined => {
             const types = Object.keys(farmsConfig[version]);
             for (const type of types) {
                 const address = farmsConfig[version][type].find(
-                    address => address === farmAddress,
+                    (address: string) => address === farmAddress,
                 );
                 if (address !== undefined) {
                     return toRewardTypeEnum(type);
