86343bd6af8a84ca5fb39ae45c6c579b741d018b
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-10-12 15:25:04 +0300

    SERVICES-756: treat query error
    if no claim progress or storage exists

--- src/services/generics/generic.abi.service.ts
+++ src/services/generics/generic.abi.service.ts
@@ -1,16 +1,12 @@
 import { ContractQueryResponse } from '@elrondnetwork/erdjs-network-providers/out';
-import {
-    Interaction,
-    Query,
-    ResultsParser,
-    TypedOutcomeBundle,
-} from '@elrondnetwork/erdjs/out';
+import { Interaction, Query, ResultsParser, TypedOutcomeBundle } from '@elrondnetwork/erdjs/out';
 import { Inject, Injectable } from '@nestjs/common';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { generateRunQueryLogMessage } from 'src/utils/generate-log-message';
 import { PendingExecutor } from 'src/utils/pending.executor';
 import { Logger } from 'winston';
 import { ElrondProxyService } from '../elrond-communication/elrond-proxy.service';
+import { ReturnCode } from '@elrondnetwork/erdjs/out/smartcontracts/returnCode';
 
 @Injectable()
 export class GenericAbiService {
@@ -33,6 +29,18 @@ export class GenericAbiService {
             const query = interaction.check().buildQuery();
             const queryResponse = await this.queryExecutor.execute(query);
             const endpointDefinition = interaction.getEndpoint();
+            if (queryResponse.returnCode === "user error"
+                && queryResponse.returnMessage === "storage decode error: input too short") {
+                return {
+                    returnCode: ReturnCode.UserError,
+                    returnMessage: queryResponse.returnMessage,
+                    firstValue: undefined,
+                    lastValue: undefined,
+                    secondValue: undefined,
+                    thirdValue: undefined,
+                    values: []
+                }
+            }
             return new ResultsParser().parseQueryResponse(
                 queryResponse,
                 endpointDefinition,
