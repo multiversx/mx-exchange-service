604319569274eaea1f3fb043e5da2afba1b2c82b
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2022-11-16 13:47:48 +0200

    SERVICES-919: remove getUpdateEnergyCacheKey

--- src/modules/user/services/userEnergy/user.energy.getter.service.ts
+++ src/modules/user/services/userEnergy/user.energy.getter.service.ts
@@ -1,7 +1,6 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { GenericGetterService } from "../../../../services/generics/generic.getter.service";
 import { oneMinute } from "../../../../helpers/helpers";
-import { generateCacheKeyFromParams } from "../../../../utils/generate-cache-key";
 import { UserEnergyComputeService } from "./user.energy.compute.service";
 import { CachingService } from "../../../../services/caching/cache.service";
 import { WINSTON_MODULE_PROVIDER } from "nest-winston";
@@ -19,13 +18,9 @@ export class UserEnergyGetterService extends GenericGetterService {
 
     async getUserEnergyOutdatedAddresses(userAddress: string): Promise<string[]> {
         return this.getData(
-            this.getUpdateEnergyCacheKey('userEnergyOutdatedAddresses', userAddress),
+            this.getCacheKey('userEnergyOutdatedAddresses', userAddress),
             () => this.userEnergyCompute.computeUserEnergyOutdatedAddresses(userAddress),
             oneMinute(),
         )
-    }
-
-    private getUpdateEnergyCacheKey(...args: any) {
-        return generateCacheKeyFromParams(...args);
     }
 }
