a741fecd5542de2c9ea1b6396f9dcd82ed146842
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-11-18 12:12:37 +0200

    SERVICES-938 fix base rewards compute on farm v2
    - farm reward per share already includes splitted percentages of rewards
    
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/farm/v2/services/farm.v2.compute.service.ts
+++ src/modules/farm/v2/services/farm.v2.compute.service.ts
@@ -1,37 +1,30 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { FarmComputeService } from '../../base-module/services/farm.compute.service';
-import BigNumber from "bignumber.js";
-import {
-    WeeklyRewardsSplittingComputeService
-} from "../../../../submodules/weekly-rewards-splitting/services/weekly-rewards-splitting.compute.service";
-import { Mixin } from "ts-mixer";
-import { EsdtTokenPayment } from "../../../../models/esdtTokenPayment.model";
-import { FarmGetterServiceV2 } from "./farm.v2.getter.service";
-import { PairComputeService } from "../../../pair/services/pair.compute.service";
-import { TokenComputeService } from "../../../tokens/services/token.compute.service";
-import { WINSTON_MODULE_PROVIDER } from "nest-winston";
-import { Logger } from "winston";
-import { constantsConfig } from "../../../../config";
-import {
-    WeekTimekeepingGetterService
-} from "../../../../submodules/week-timekeeping/services/week-timekeeping.getter.service";
-import {
-    WeekTimekeepingComputeService
-} from "../../../../submodules/week-timekeeping/services/week-timekeeping.compute.service";
-import {
-    WeeklyRewardsSplittingGetterService
-} from "../../../../submodules/weekly-rewards-splitting/services/weekly-rewards-splitting.getter.service";
-import {
-    ProgressComputeService
-} from "../../../../submodules/weekly-rewards-splitting/services/progress.compute.service";
-import { EnergyGetterService } from "../../../energy/services/energy.getter.service";
-import { CalculateRewardsArgs } from "../../models/farm.args";
-import { PairService } from "../../../pair/services/pair.service";
-import { PairGetterService } from "../../../pair/services/pair.getter.service";
-import { ContextGetterService } from "../../../../services/context/context.getter.service";
+import BigNumber from 'bignumber.js';
+import { WeeklyRewardsSplittingComputeService } from '../../../../submodules/weekly-rewards-splitting/services/weekly-rewards-splitting.compute.service';
+import { Mixin } from 'ts-mixer';
+import { EsdtTokenPayment } from '../../../../models/esdtTokenPayment.model';
+import { FarmGetterServiceV2 } from './farm.v2.getter.service';
+import { PairComputeService } from '../../../pair/services/pair.compute.service';
+import { TokenComputeService } from '../../../tokens/services/token.compute.service';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+import { constantsConfig } from '../../../../config';
+import { WeekTimekeepingGetterService } from '../../../../submodules/week-timekeeping/services/week-timekeeping.getter.service';
+import { WeekTimekeepingComputeService } from '../../../../submodules/week-timekeeping/services/week-timekeeping.compute.service';
+import { WeeklyRewardsSplittingGetterService } from '../../../../submodules/weekly-rewards-splitting/services/weekly-rewards-splitting.getter.service';
+import { ProgressComputeService } from '../../../../submodules/weekly-rewards-splitting/services/progress.compute.service';
+import { EnergyGetterService } from '../../../energy/services/energy.getter.service';
+import { CalculateRewardsArgs } from '../../models/farm.args';
+import { PairService } from '../../../pair/services/pair.service';
+import { PairGetterService } from '../../../pair/services/pair.getter.service';
+import { ContextGetterService } from '../../../../services/context/context.getter.service';
 
 @Injectable()
-export class FarmComputeServiceV2 extends Mixin(FarmComputeService, WeeklyRewardsSplittingComputeService) {
+export class FarmComputeServiceV2 extends Mixin(
+    FarmComputeService,
+    WeeklyRewardsSplittingComputeService,
+) {
     constructor(
         @Inject(forwardRef(() => FarmGetterServiceV2))
         protected readonly farmGetter: FarmGetterServiceV2,
@@ -48,7 +41,15 @@ export class FarmComputeServiceV2 extends Mixin(FarmComputeService, WeeklyReward
         protected readonly contextGetter: ContextGetterService,
         @Inject(WINSTON_MODULE_PROVIDER) protected readonly logger: Logger,
     ) {
-        super(weekTimekeepingGetter, weekTimekeepingCompute, weeklyRewardsSplittingGetter, progressCompute, pairCompute, energyGetter, tokenCompute);
+        super(
+            weekTimekeepingGetter,
+            weekTimekeepingCompute,
+            weeklyRewardsSplittingGetter,
+            progressCompute,
+            pairCompute,
+            energyGetter,
+            tokenCompute,
+        );
     }
 
     async computeFarmBaseAPR(farmAddress: string): Promise<string> {
@@ -66,9 +67,10 @@ export class FarmComputeServiceV2 extends Mixin(FarmComputeService, WeeklyReward
             this.computeFarmedTokenPriceUSD(farmAddress),
         ]);
 
-        const apr = this.computeBaseRewards(new BigNumber(totalRewardsPerYearUSD), boostedYieldsRewardsPercenatage)
-            .div(farmTokenSupplyUSD,
-            );
+        const apr = this.computeBaseRewards(
+            new BigNumber(totalRewardsPerYearUSD),
+            boostedYieldsRewardsPercenatage,
+        ).div(farmTokenSupplyUSD);
 
         let feesAPR: BigNumber = new BigNumber(0);
         if (farmedTokenID !== farmingTokenID) {
@@ -88,23 +90,25 @@ export class FarmComputeServiceV2 extends Mixin(FarmComputeService, WeeklyReward
         positon: CalculateRewardsArgs,
         rewardPerShare: string,
     ): Promise<BigNumber> {
-
-        const [totalFarmRewards, boostedYieldsRewardsPercenatage] = await Promise.all([
-            super.computeFarmRewardsForPosition(positon, rewardPerShare),
-            this.farmGetter.getBoostedYieldsRewardsPercenatage(positon.farmAddress)
-        ]);
-        return this.computeBaseRewards(totalFarmRewards, boostedYieldsRewardsPercenatage);
+        return await super.computeFarmRewardsForPosition(
+            positon,
+            rewardPerShare,
+        );
     }
 
-    computeBaseRewards(totalFarmRewards: BigNumber, boostedYieldsRewardsPercenatage: number): BigNumber {
-        const boostedYieldsRewardsPercenatageBig = new BigNumber(boostedYieldsRewardsPercenatage);
+    computeBaseRewards(
+        totalFarmRewards: BigNumber,
+        boostedYieldsRewardsPercenatage: number,
+    ): BigNumber {
+        const boostedYieldsRewardsPercenatageBig = new BigNumber(
+            boostedYieldsRewardsPercenatage,
+        );
 
         if (boostedYieldsRewardsPercenatageBig.isPositive()) {
             const boosterFarmRewardsCut = totalFarmRewards
                 .multipliedBy(boostedYieldsRewardsPercenatageBig)
-                .dividedBy(constantsConfig.MAX_PERCENT)
-            return totalFarmRewards
-                .minus(boosterFarmRewardsCut)
+                .dividedBy(constantsConfig.MAX_PERCENT);
+            return totalFarmRewards.minus(boosterFarmRewardsCut);
         }
     }
 
@@ -115,17 +119,21 @@ export class FarmComputeServiceV2 extends Mixin(FarmComputeService, WeeklyReward
         energyAmount: string,
         liquidity: string,
     ): Promise<EsdtTokenPayment[]> {
-
         const payments: EsdtTokenPayment[] = [];
 
-        const boostedYieldsFactors = await this.farmGetter.getBoostedYieldsFactors(scAddress);
+        const boostedYieldsFactors =
+            await this.farmGetter.getBoostedYieldsFactors(scAddress);
 
-        const userHasMinEnergy = new BigNumber(energyAmount).isGreaterThan(boostedYieldsFactors.minEnergyAmount);
+        const userHasMinEnergy = new BigNumber(energyAmount).isGreaterThan(
+            boostedYieldsFactors.minEnergyAmount,
+        );
         if (!userHasMinEnergy) {
             return payments;
         }
 
-        const userMinFarmAmount = new BigNumber(liquidity).isGreaterThan(boostedYieldsFactors.minFarmAmount);
+        const userMinFarmAmount = new BigNumber(liquidity).isGreaterThan(
+            boostedYieldsFactors.minFarmAmount,
+        );
         if (!userMinFarmAmount) {
             return payments;
         }
@@ -139,44 +147,53 @@ export class FarmComputeServiceV2 extends Mixin(FarmComputeService, WeeklyReward
             return payments;
         }
 
-        const [
-            rewardsPerBlock,
-            farmTokenSupply,
-            totalEnergy,
-            userEnergy
-        ] = await Promise.all([
-            this.farmGetter.getRewardsPerBlock(scAddress),
-            this.farmGetter.getFarmTokenSupply(scAddress),
-            this.weeklyRewardsSplittingGetter.totalEnergyForWeek(scAddress, week),
-            this.weeklyRewardsSplittingGetter.userEnergyForWeek(scAddress, userAddress, week)
-        ]);
+        const [rewardsPerBlock, farmTokenSupply, totalEnergy, userEnergy] =
+            await Promise.all([
+                this.farmGetter.getRewardsPerBlock(scAddress),
+                this.farmGetter.getFarmTokenSupply(scAddress),
+                this.weeklyRewardsSplittingGetter.totalEnergyForWeek(
+                    scAddress,
+                    week,
+                ),
+                this.weeklyRewardsSplittingGetter.userEnergyForWeek(
+                    scAddress,
+                    userAddress,
+                    week,
+                ),
+            ]);
 
         const userBaseRewardsPerBlock = new BigNumber(rewardsPerBlock)
             .multipliedBy(liquidity)
-            .dividedBy(farmTokenSupply)
-        const userRewardsForWeek = new BigNumber(boostedYieldsFactors.userRewardsBase)
+            .dividedBy(farmTokenSupply);
+        const userRewardsForWeek = new BigNumber(
+            boostedYieldsFactors.userRewardsBase,
+        )
             .multipliedBy(userBaseRewardsPerBlock)
-            .multipliedBy(constantsConfig.BLOCKS_PER_WEEK)
+            .multipliedBy(constantsConfig.BLOCKS_PER_WEEK);
 
         for (const weeklyRewards of totalRewards) {
             const boostedRewardsByEnergy = new BigNumber(weeklyRewards.amount)
                 .multipliedBy(boostedYieldsFactors.userRewardsEnergy)
                 .multipliedBy(userEnergy.amount)
-                .dividedBy(totalEnergy)
+                .dividedBy(totalEnergy);
 
             const boostedRewardsByTokens = new BigNumber(weeklyRewards.amount)
                 .multipliedBy(boostedYieldsFactors.userRewardsFarm)
                 .multipliedBy(liquidity)
-                .dividedBy(farmTokenSupply)
+                .dividedBy(farmTokenSupply);
 
-            const constantsBase = new BigNumber(boostedYieldsFactors.userRewardsEnergy)
-                .plus(boostedYieldsFactors.userRewardsFarm)
+            const constantsBase = new BigNumber(
+                boostedYieldsFactors.userRewardsEnergy,
+            ).plus(boostedYieldsFactors.userRewardsFarm);
 
-            const boostedRewardAmount = (boostedRewardsByEnergy
-                .plus(boostedRewardsByTokens))
-                .dividedBy(constantsBase)
+            const boostedRewardAmount = boostedRewardsByEnergy
+                .plus(boostedRewardsByTokens)
+                .dividedBy(constantsBase);
 
-            const paymentAmount = boostedRewardAmount.comparedTo(userRewardsForWeek) < 1 ? boostedRewardAmount : userRewardsForWeek;
+            const paymentAmount =
+                boostedRewardAmount.comparedTo(userRewardsForWeek) < 1
+                    ? boostedRewardAmount
+                    : userRewardsForWeek;
             if (paymentAmount.isPositive()) {
                 const payment = new EsdtTokenPayment();
                 payment.amount = paymentAmount.toFixed();
