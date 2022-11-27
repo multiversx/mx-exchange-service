b4ee7d3fcd4c38dd7d200921a3f910275b4834f3
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-11-27 22:19:39 +0200

    SERVICES-945 add setter service for token unstake module
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/modules/token-unstake/services/token.unstake.setter.service.ts
@@ -0,0 +1,29 @@
+import { Inject } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { CachingService } from 'src/services/caching/cache.service';
+import { CacheTtlInfo } from 'src/services/caching/cache.ttl.info';
+import { GenericSetterService } from 'src/services/generics/generic.setter.service';
+import { Logger } from 'winston';
+import { UnstakePairModel } from '../models/token.unstake.model';
+
+export class TokenUnstakeSetterService extends GenericSetterService {
+    constructor(
+        protected readonly cachingService: CachingService,
+        @Inject(WINSTON_MODULE_PROVIDER) protected readonly logger: Logger,
+    ) {
+        super(cachingService, logger);
+        this.baseKey = 'tokenUnstake';
+    }
+
+    async setUnlockedTokensForUser(
+        userAddress: string,
+        value: UnstakePairModel[],
+    ): Promise<string> {
+        return await this.setData(
+            this.getCacheKey(userAddress, 'unlockedTokens'),
+            value,
+            CacheTtlInfo.ContractBalance.remoteTtl,
+            CacheTtlInfo.ContractBalance.localTtl,
+        );
+    }
+}
