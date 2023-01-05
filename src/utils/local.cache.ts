db879534e45f6a41384ae4901e3ea874740a96af
Florin Cozloschi <115977960+florincoz@users.noreply.github.com>
2023-01-05 19:48:44 +0200

    add maximum local cache size in config

--- src/utils/local.cache.ts
+++ src/utils/local.cache.ts
@@ -1,7 +1,9 @@
 import LRU from 'lru-cache';
+import { elrondConfig } from '../config';
+
 
 export default new LRU({
-  max: 10000,
+  max: elrondConfig.localCacheMaxItems,
   // for use with tracking overall storage size
   allowStale: false,
   updateAgeOnGet: false,
