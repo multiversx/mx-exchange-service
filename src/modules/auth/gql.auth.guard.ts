76bc8dd140f3c74bdb52a4ff60a0f937ced6d713
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2023-01-17 14:12:28 +0200

    SERVICES-1182 change GqlAuthGuard implementation
    - implements CanActivate instead of using a JwtStrategy
    
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/auth/gql.auth.guard.ts
+++ src/modules/auth/gql.auth.guard.ts
@@ -1,27 +1,25 @@
 import {
+    CanActivate,
     ExecutionContext,
-    ForbiddenException,
-    HttpException,
-    HttpStatus,
     Inject,
     Injectable,
 } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
+import { ApiConfigService } from 'src/helpers/api.config.service';
+import { JwtPayload, verify } from 'jsonwebtoken';
 
 @Injectable()
-export class GqlAuthGuard extends AuthGuard('jwt') {
+export class GqlAuthGuard implements CanActivate {
     private impersonateAddress: string;
 
     constructor(
+        private readonly apiConfigService: ApiConfigService,
         @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
-    ) {
-        super();
-    }
-    canActivate(context: ExecutionContext) {
+    ) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
         const ctx = GqlExecutionContext.create(context);
         const { req } = ctx.getContext();
         this.logger.info('request header', [
@@ -31,31 +29,44 @@ export class GqlAuthGuard extends AuthGuard('jwt') {
         if (req.headers !== undefined) {
             this.impersonateAddress = req.headers['impersonate-address'];
         }
-        return super.canActivate(new ExecutionContextHost([req]));
-    }
 
-    handleRequest(err: any, user: any, info: any) {
-        if (!err && !!user) {
-            this.logger.info('address', [{ user: user }]);
-            if (this.impersonateAddress) {
-                const admins = process.env.SECURITY_ADMINS.split(',');
-                if (admins.find(admin => admin === user.publicKey)) {
-                    user.publicKey = this.impersonateAddress;
-                }
-            }
-            return user;
+        const authorization: string = req.headers['authorization'];
+        if (!authorization) {
+            return false;
+        }
+        const jwt = authorization.replace('Bearer ', '');
+
+        try {
+            const jwtSecret = this.apiConfigService.getJwtSecret();
+
+            req.jwt = await new Promise((resolve, reject) => {
+                verify(jwt, jwtSecret, (err, decoded: JwtPayload) => {
+                    if (err) {
+                        reject(err);
+                    }
+
+                    req.auth = {
+                        address: decoded.sub,
+                        expires: decoded.exp,
+                        issued: decoded.iat,
+                        host: decoded.iss,
+                    };
+                    if (this.impersonateAddress) {
+                        const admins = process.env.SECURITY_ADMINS.split(',');
+                        if (
+                            admins.find((admin) => admin === req.auth.address)
+                        ) {
+                            req.auth.address = this.impersonateAddress;
+                        }
+                    }
+                    resolve(req.auth);
+                });
+            });
+        } catch (error) {
+            this.logger.error(error);
+            return false;
         }
 
-        if (info?.name === 'TokenExpiredError') {
-            throw new ForbiddenException('token expired');
-        }
-
-        if (info?.name === 'JsonWebTokenError') {
-            throw new ForbiddenException('invalid token');
-        }
-
-        throw new ForbiddenException(
-            'You are not authorized to make this request',
-        );
+        return true;
     }
 }
