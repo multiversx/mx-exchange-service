36e2b3f06db24ecad8a6f5fb1e8fc039b7fac4fa
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2023-01-20 16:38:15 +0200

    fix wrong config name
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/services/analytics/data-api/data-api.query.service.ts
+++ src/services/analytics/data-api/data-api.query.service.ts
@@ -1,123 +1,160 @@
-import { AggregateValue, DataApiClient, DataApiQueryBuilder, HistoricalValue, TimeRange, TimeResolution } from '@multiversx/sdk-data-api-client';
+import {
+    AggregateValue,
+    DataApiClient,
+    DataApiQueryBuilder,
+    HistoricalValue,
+    TimeRange,
+    TimeResolution,
+} from '@multiversx/sdk-data-api-client';
 import { Inject, Injectable } from '@nestjs/common';
 import BigNumber from 'bignumber.js';
 import fs from 'fs';
 import moment from 'moment';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
-import { dataApiConfig, elrondConfig } from 'src/config';
+import { dataApiConfig, mxConfig } from 'src/config';
 import { ApiConfigService } from 'src/helpers/api.config.service';
 import { HistoricDataModel } from 'src/modules/analytics/models/analytics.model';
-import { computeTimeInterval, convertBinToTimeResolution, DataApiQuery } from 'src/utils/analytics.utils';
+import {
+    computeTimeInterval,
+    convertBinToTimeResolution,
+    DataApiQuery,
+} from 'src/utils/analytics.utils';
 import { Logger } from 'winston';
 import { AnalyticsQueryArgs } from '../entities/analytics.query.args';
 import { AnalyticsQueryInterface } from '../interfaces/analytics.query.interface';
 
 @Injectable()
 export class DataApiQueryService implements AnalyticsQueryInterface {
-  private readonly dataApiClient: DataApiClient;
+    private readonly dataApiClient: DataApiClient;
 
-  constructor(
-    private readonly apiConfigService: ApiConfigService,
-    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
-  ) {
-    this.dataApiClient = new DataApiClient({
-      host: 'dex-service',
-      dataApiUrl: process.env.ELRONDDATAAPI_URL,
-      multiversXApiUrl: this.apiConfigService.getApiUrl(),
-      proxyTimeout: elrondConfig.proxyTimeout,
-      keepAlive: {
-        maxSockets: elrondConfig.keepAliveMaxSockets,
-        maxFreeSockets: elrondConfig.keepAliveMaxFreeSockets,
-        timeout: this.apiConfigService.getKeepAliveTimeoutDownstream(),
-        freeSocketTimeout: elrondConfig.keepAliveFreeSocketTimeout,
-      },
-      signerPrivateKey: fs.readFileSync(this.apiConfigService.getNativeAuthKeyPath()).toString(),
-    });
-  }
+    constructor(
+        private readonly apiConfigService: ApiConfigService,
+        @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+    ) {
+        this.dataApiClient = new DataApiClient({
+            host: 'dex-service',
+            dataApiUrl: process.env.ELRONDDATAAPI_URL,
+            multiversXApiUrl: this.apiConfigService.getApiUrl(),
+            proxyTimeout: mxConfig.proxyTimeout,
+            keepAlive: {
+                maxSockets: mxConfig.keepAliveMaxSockets,
+                maxFreeSockets: mxConfig.keepAliveMaxFreeSockets,
+                timeout: this.apiConfigService.getKeepAliveTimeoutDownstream(),
+                freeSocketTimeout: mxConfig.keepAliveFreeSocketTimeout,
+            },
+            signerPrivateKey: fs
+                .readFileSync(this.apiConfigService.getNativeAuthKeyPath())
+                .toString(),
+        });
+    }
 
-  @DataApiQuery()
-  async getAggregatedValue({ series, metric, time }: AnalyticsQueryArgs): Promise<string> {
-    const [startDate, endDate] = computeTimeInterval(time);
+    @DataApiQuery()
+    async getAggregatedValue({
+        series,
+        metric,
+        time,
+    }: AnalyticsQueryArgs): Promise<string> {
+        const [startDate, endDate] = computeTimeInterval(time);
 
-    const query = DataApiQueryBuilder
-      .createXExchangeAnalyticsQuery()
-      .metric(series, metric)
-      .betweenDates(startDate, endDate)
-      .getAggregate(AggregateValue.sum);
+        const query = DataApiQueryBuilder.createXExchangeAnalyticsQuery()
+            .metric(series, metric)
+            .betweenDates(startDate, endDate)
+            .getAggregate(AggregateValue.sum);
 
-    const data = await this.dataApiClient.executeAggregateQuery(query);
+        const data = await this.dataApiClient.executeAggregateQuery(query);
 
-    const value = new BigNumber(data?.sum ?? '0').toFixed();
-    return value;
-  }
+        const value = new BigNumber(data?.sum ?? '0').toFixed();
+        return value;
+    }
 
-  @DataApiQuery()
-  async getLatestCompleteValues({ series, metric }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
-    const query = DataApiQueryBuilder
-      .createXExchangeAnalyticsQuery()
-      .metric(series, metric)
-      .withTimeRange(TimeRange.ALL)
-      .withTimeResolution(TimeResolution.INTERVAL_DAY)
-      .getHistorical(HistoricalValue.last, HistoricalValue.time);
+    @DataApiQuery()
+    async getLatestCompleteValues({
+        series,
+        metric,
+    }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
+        const query = DataApiQueryBuilder.createXExchangeAnalyticsQuery()
+            .metric(series, metric)
+            .withTimeRange(TimeRange.ALL)
+            .withTimeResolution(TimeResolution.INTERVAL_DAY)
+            .getHistorical(HistoricalValue.last, HistoricalValue.time);
 
-    const rows = await this.dataApiClient.executeHistoricalQuery(query);
+        const rows = await this.dataApiClient.executeHistoricalQuery(query);
 
-    const data = rows.map((row) => HistoricDataModel.fromDataApiResponse(row, HistoricalValue.last));
-    return data;
-  }
+        const data = rows.map((row) =>
+            HistoricDataModel.fromDataApiResponse(row, HistoricalValue.last),
+        );
+        return data;
+    }
 
-  @DataApiQuery()
-  async getSumCompleteValues({ series, metric }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
-    const query = DataApiQueryBuilder
-      .createXExchangeAnalyticsQuery()
-      .metric(series, metric)
-      .withTimeRange(TimeRange.ALL)
-      .withTimeResolution(TimeResolution.INTERVAL_DAY)
-      .fillDataGaps({ skipFirstNullValues: true })
-      .getHistorical(HistoricalValue.sum, HistoricalValue.time);
+    @DataApiQuery()
+    async getSumCompleteValues({
+        series,
+        metric,
+    }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
+        const query = DataApiQueryBuilder.createXExchangeAnalyticsQuery()
+            .metric(series, metric)
+            .withTimeRange(TimeRange.ALL)
+            .withTimeResolution(TimeResolution.INTERVAL_DAY)
+            .fillDataGaps({ skipFirstNullValues: true })
+            .getHistorical(HistoricalValue.sum, HistoricalValue.time);
 
-    const rows = await this.dataApiClient.executeHistoricalQuery(query);
+        const rows = await this.dataApiClient.executeHistoricalQuery(query);
 
-    const data = rows.map((row) => HistoricDataModel.fromDataApiResponse(row, HistoricalValue.sum));
-    return data;
-  }
+        const data = rows.map((row) =>
+            HistoricDataModel.fromDataApiResponse(row, HistoricalValue.sum),
+        );
+        return data;
+    }
 
-  @DataApiQuery()
-  async getValues24h({ series, metric }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
-    const query = DataApiQueryBuilder
-      .createXExchangeAnalyticsQuery()
-      .metric(series, metric)
-      .withTimeRange(TimeRange.DAY)
-      .withTimeResolution(TimeResolution.INTERVAL_HOUR)
-      .getHistorical(HistoricalValue.last, HistoricalValue.time);
+    @DataApiQuery()
+    async getValues24h({
+        series,
+        metric,
+    }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
+        const query = DataApiQueryBuilder.createXExchangeAnalyticsQuery()
+            .metric(series, metric)
+            .withTimeRange(TimeRange.DAY)
+            .withTimeResolution(TimeResolution.INTERVAL_HOUR)
+            .getHistorical(HistoricalValue.last, HistoricalValue.time);
 
-    const rows = await this.dataApiClient.executeHistoricalQuery(query);
+        const rows = await this.dataApiClient.executeHistoricalQuery(query);
 
-    const data = rows.map((row) => HistoricDataModel.fromDataApiResponse(row, HistoricalValue.last));
-    return data;
-  }
+        const data = rows.map((row) =>
+            HistoricDataModel.fromDataApiResponse(row, HistoricalValue.last),
+        );
+        return data;
+    }
 
-  @DataApiQuery()
-  async getValues24hSum({ series, metric }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
-    const query = DataApiQueryBuilder
-      .createXExchangeAnalyticsQuery()
-      .metric(series, metric)
-      .withTimeRange(TimeRange.DAY)
-      .withTimeResolution(TimeResolution.INTERVAL_HOUR)
-      .fillDataGaps()
-      .getHistorical(HistoricalValue.sum, HistoricalValue.time);
+    @DataApiQuery()
+    async getValues24hSum({
+        series,
+        metric,
+    }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
+        const query = DataApiQueryBuilder.createXExchangeAnalyticsQuery()
+            .metric(series, metric)
+            .withTimeRange(TimeRange.DAY)
+            .withTimeResolution(TimeResolution.INTERVAL_HOUR)
+            .fillDataGaps()
+            .getHistorical(HistoricalValue.sum, HistoricalValue.time);
 
-    const rows = await this.dataApiClient.executeHistoricalQuery(query);
+        const rows = await this.dataApiClient.executeHistoricalQuery(query);
 
-    const data = rows.map((row) => HistoricDataModel.fromDataApiResponse(row, HistoricalValue.sum));
-    return data;
-  }
+        const data = rows.map((row) =>
+            HistoricDataModel.fromDataApiResponse(row, HistoricalValue.sum),
+        );
+        return data;
+    }
 
-  @DataApiQuery()
-  async getLatestHistoricData({ time, series, metric, start }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
-    const [startDate, endDate] = computeTimeInterval(time, start);
+    @DataApiQuery()
+    async getLatestHistoricData({
+        time,
+        series,
+        metric,
+        start,
+    }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
+        const [startDate, endDate] = computeTimeInterval(time, start);
 
-    const query = `query getLatestHistoricData($series: String!, $metric: String!, $startDate: DateTime!, $endDate: DateTime!) {
+        const query = `query getLatestHistoricData($series: String!, $metric: String!, $startDate: DateTime!, $endDate: DateTime!) {
       ${dataApiConfig.tableName} {
         values(
           series: $series
@@ -130,39 +167,52 @@ export class DataApiQueryService implements AnalyticsQueryInterface {
       }
     }`;
 
-    const variables = {
-      series,
-      metric,
-      startDate: moment.utc(startDate).format('YYYY-MM-DD HH:mm:ss'),
-      endDate: moment.utc(endDate).format('YYYY-MM-DD HH:mm:ss'),
+        const variables = {
+            series,
+            metric,
+            startDate: moment.utc(startDate).format('YYYY-MM-DD HH:mm:ss'),
+            endDate: moment.utc(endDate).format('YYYY-MM-DD HH:mm:ss'),
+        };
+
+        const response = await this.dataApiClient.executeRawQuery({
+            query,
+            variables,
+        });
+        const rows = response.xExchangeAnalytics.values;
+
+        const data = rows.map(
+            (row: any) =>
+                new HistoricDataModel({
+                    timestamp: moment.utc(row.time).unix().toString(),
+                    value: new BigNumber(row.value ?? '0').toFixed(),
+                }),
+        );
+        return data;
     }
 
-    const response = await this.dataApiClient.executeRawQuery({ query, variables });
-    const rows = response.xExchangeAnalytics.values
+    @DataApiQuery()
+    async getLatestBinnedHistoricData({
+        time,
+        series,
+        metric,
+        start,
+        bin,
+    }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
+        const [startDate, endDate] = computeTimeInterval(time, start);
+        const timeResolution = convertBinToTimeResolution(bin);
 
-    const data = rows.map((row: any) => new HistoricDataModel({
-      timestamp: moment.utc(row.time).unix().toString(),
-      value: new BigNumber(row.value ?? '0').toFixed(),
-    }));
-    return data;
-  }
+        const query = DataApiQueryBuilder.createXExchangeAnalyticsQuery()
+            .metric(series, metric)
+            .betweenDates(startDate, endDate)
+            .withTimeResolution(timeResolution)
+            .fillDataGaps()
+            .getHistorical(HistoricalValue.avg, HistoricalValue.time);
 
-  @DataApiQuery()
-  async getLatestBinnedHistoricData({ time, series, metric, start, bin }: AnalyticsQueryArgs): Promise<HistoricDataModel[]> {
-    const [startDate, endDate] = computeTimeInterval(time, start);
-    const timeResolution = convertBinToTimeResolution(bin);
+        const rows = await this.dataApiClient.executeHistoricalQuery(query);
 
-    const query = DataApiQueryBuilder
-      .createXExchangeAnalyticsQuery()
-      .metric(series, metric)
-      .betweenDates(startDate, endDate)
-      .withTimeResolution(timeResolution)
-      .fillDataGaps()
-      .getHistorical(HistoricalValue.avg, HistoricalValue.time);
-
-    const rows = await this.dataApiClient.executeHistoricalQuery(query);
-
-    const data = rows.map((row) => HistoricDataModel.fromDataApiResponse(row, HistoricalValue.max));
-    return data;
-  }
+        const data = rows.map((row) =>
+            HistoricDataModel.fromDataApiResponse(row, HistoricalValue.max),
+        );
+        return data;
+    }
 }
