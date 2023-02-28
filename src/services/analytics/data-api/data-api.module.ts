d09a061bbcbc0dc3b320eac672643b3078a218e7
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2023-02-28 11:34:19 +0200

    SERVICES-1295 add postgres connection and entities
    - add typeorm connection to timescaleDB
    - add dex analytics entities
    
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/services/analytics/data-api/data-api.module.ts
+++ src/services/analytics/data-api/data-api.module.ts
@@ -1,12 +1,50 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonAppModule } from 'src/common.app.module';
+import { ApiConfigService } from 'src/helpers/api.config.service';
 import { CachingModule } from 'src/services/caching/cache.module';
 import { DataApiQueryService } from './data-api.query.service';
 import { DataApiWriteService } from './data-api.write.service';
+import {
+    CloseDaily,
+    CloseHourly,
+    SumDaily,
+    SumHourly,
+    XExchangeAnalyticsEntity,
+} from './entities/data.api.entities';
 
 @Module({
-    imports: [CommonAppModule, CachingModule],
+    imports: [
+        CommonAppModule,
+        CachingModule,
+        TypeOrmModule.forRootAsync({
+            imports: [CommonAppModule],
+            useFactory: (apiConfig: ApiConfigService) => ({
+                type: 'postgres',
+                host: apiConfig.getTimescaleDbHost(),
+                port: apiConfig.getTimescaleDbPort(),
+                database: apiConfig.getTimescaleDbDatabase(),
+                username: apiConfig.getTimescaleDbUsername(),
+                password: apiConfig.getTimescaleDbPassword(),
+                ssl: true,
+                extra: {
+                    ssl: {
+                        rejectUnauthorized: false,
+                    },
+                },
+                entities: ['dist/**/*.entities.{ts,js}'],
+            }),
+            inject: [ApiConfigService],
+        }),
+        TypeOrmModule.forFeature([
+            XExchangeAnalyticsEntity,
+            SumDaily,
+            SumHourly,
+            CloseDaily,
+            CloseHourly,
+        ]),
+    ],
     providers: [DataApiQueryService, DataApiWriteService],
     exports: [DataApiQueryService, DataApiWriteService],
 })
-export class DataApiModule { }
+export class DataApiModule {}
