1d73cc45f36ec01ad3340dca574e7835a0044a8f
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2023-04-10 10:24:23 +0300

    SERVICES-1513: remove try catch from distribution resolver
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/distribution/distribution.resolver.ts
+++ src/modules/distribution/distribution.resolver.ts
@@ -1,13 +1,11 @@
 import { Resolver, Query, ResolveField } from '@nestjs/graphql';
 import { UseGuards } from '@nestjs/common';
 import { TransactionModel } from '../../models/transaction.model';
-import { DistributionService } from './services/distribution.service';
 import {
     CommunityDistributionModel,
     DistributionModel,
 } from './models/distribution.model';
 import { DistributionTransactionsService } from './services/distribution.transactions.service';
-import { ApolloError } from 'apollo-server-express';
 import { AuthUser } from '../auth/auth.user';
 import { UserAuthResult } from '../auth/user.auth.result';
 import { JwtOrNativeAuthGuard } from '../auth/jwt.or.native.auth.guard';
@@ -17,39 +15,26 @@ import { scAddress } from 'src/config';
 @Resolver(() => DistributionModel)
 export class DistributionResolver {
     constructor(
-        private readonly distributionService: DistributionService,
         private readonly distributionAbi: DistributionAbiService,
         private readonly transactionsService: DistributionTransactionsService,
     ) {}
 
     @ResolveField()
     async communityDistribution(): Promise<CommunityDistributionModel> {
-        try {
-            return await this.distributionAbi.communityDistribution();
-        } catch (error) {
-            throw new ApolloError(error);
-        }
+        return this.distributionAbi.communityDistribution();
     }
 
     @Query(() => DistributionModel)
     async distribution(): Promise<DistributionModel> {
-        try {
-            return new DistributionModel({
-                address: scAddress.distributionAddress,
-            });
-        } catch (error) {
-            throw new ApolloError(error);
-        }
+        return new DistributionModel({
+            address: scAddress.distributionAddress,
+        });
     }
 
     @UseGuards(JwtOrNativeAuthGuard)
     @Query(() => TransactionModel)
     async claimLockedAssets(): Promise<TransactionModel> {
-        try {
-            return await this.transactionsService.claimLockedAssets();
-        } catch (error) {
-            throw new ApolloError(error);
-        }
+        return this.transactionsService.claimLockedAssets();
     }
 
     @UseGuards(JwtOrNativeAuthGuard)
@@ -57,13 +42,9 @@ export class DistributionResolver {
     async distributedLockedAssets(
         @AuthUser() user: UserAuthResult,
     ): Promise<string> {
-        try {
-            const assets = await this.distributionAbi.distributedLockedAssets(
-                user.address,
-            );
-            return assets.toFixed();
-        } catch (error) {
-            throw new ApolloError(error);
-        }
+        const assets = await this.distributionAbi.distributedLockedAssets(
+            user.address,
+        );
+        return assets.toFixed();
     }
 }
