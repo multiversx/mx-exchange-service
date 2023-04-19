10009bcdd5609665c2de862c43c4a9f4d1b56838
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2023-04-19 17:02:30 +0300

    SERVICES-1546: add unit tests for locked token wrapper transaction service
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

---
+++ src/modules/locked-token-wrapper/specs/locked.token.wrapper.transaction.service.spec.ts
@@ -0,0 +1,91 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { LockedTokenWrapperTransactionService } from '../services/locked-token-wrapper.transaction.service';
+import { MXProxyServiceProvider } from 'src/services/multiversx-communication/mx.proxy.service.mock';
+import { CommonAppModule } from 'src/common.app.module';
+import { Address } from '@multiversx/sdk-core/out';
+import { TransactionModel } from 'src/models/transaction.model';
+import { gasConfig, mxConfig, scAddress } from 'src/config';
+import { encodeTransactionData } from 'src/helpers/helpers';
+
+describe('LockedTokenWrapperTransactionService', () => {
+    let module: TestingModule;
+
+    beforeEach(async () => {
+        module = await Test.createTestingModule({
+            imports: [CommonAppModule],
+            providers: [
+                LockedTokenWrapperTransactionService,
+                MXProxyServiceProvider,
+            ],
+        }).compile();
+    });
+
+    it('should be defined', () => {
+        const service = module.get<LockedTokenWrapperTransactionService>(
+            LockedTokenWrapperTransactionService,
+        );
+        expect(service).toBeDefined();
+    });
+
+    it('should get unwrapLockedToken transaction', async () => {
+        const service = module.get<LockedTokenWrapperTransactionService>(
+            LockedTokenWrapperTransactionService,
+        );
+        const transaction = await service.unwrapLockedToken(
+            Address.Zero().bech32(),
+            {
+                tokenID: 'WXMEX-123456',
+                nonce: 1,
+                amount: '1',
+            },
+        );
+        expect(transaction).toEqual(
+            new TransactionModel({
+                chainID: mxConfig.chainID,
+                nonce: 0,
+                receiver: Address.Zero().bech32(),
+                sender: Address.Zero().bech32(),
+                gasLimit: gasConfig.lockedTokenWrapper.unwrapLockedToken,
+                gasPrice: 1000000000,
+                value: '0',
+                data: encodeTransactionData(
+                    `ESDTNFTTransfer@WXMEX-123456@1@1@${scAddress.lockedTokenWrapper}@unwrapLockedToken`,
+                ),
+                options: undefined,
+                signature: undefined,
+                version: 1,
+            }),
+        );
+    });
+
+    it('should get wrapLockedToken transaction', async () => {
+        const service = module.get<LockedTokenWrapperTransactionService>(
+            LockedTokenWrapperTransactionService,
+        );
+        const transaction = await service.wrapLockedToken(
+            Address.Zero().bech32(),
+            {
+                tokenID: 'XMEX-123456',
+                nonce: 1,
+                amount: '1',
+            },
+        );
+        expect(transaction).toEqual(
+            new TransactionModel({
+                chainID: mxConfig.chainID,
+                nonce: 0,
+                receiver: Address.Zero().bech32(),
+                sender: Address.Zero().bech32(),
+                gasLimit: gasConfig.lockedTokenWrapper.wrapLockedToken,
+                gasPrice: 1000000000,
+                value: '0',
+                data: encodeTransactionData(
+                    `ESDTNFTTransfer@XMEX-123456@1@1@${scAddress.lockedTokenWrapper}@wrapLockedToken`,
+                ),
+                options: undefined,
+                signature: undefined,
+                version: 1,
+            }),
+        );
+    });
+});
