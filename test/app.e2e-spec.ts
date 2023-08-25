c07e6c95e4ae60e125af227002d1587bdfa0ff3a
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2023-08-25 11:49:56 +0300

    MEX-357: fix supertest import
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import request = require('supertest');
 import { PublicAppModule } from '../src/public.app.module';
+import * as request from 'supertest';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
@@ -16,7 +16,8 @@ describe('AppController (e2e)', () => {
     });
 
     it('/ (GET)', () => {
-        return request(app.getHttpServer())
+        return request
+            .default(app.getHttpServer())
             .get('/')
             .expect(200)
             .expect('Hello World!');
