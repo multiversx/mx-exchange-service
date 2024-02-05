99c73a8645052fc8da4ae8dc7df7a93a7fe3ff61
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2024-02-05 12:28:35 +0200

    SERVICES-2105: add mock userNfts for context getter service
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/services/context/mocks/context.getter.service.mock.ts
+++ src/services/context/mocks/context.getter.service.mock.ts
@@ -1,12 +1,32 @@
+import { MXApiService } from 'src/services/multiversx-communication/mx.api.service';
 import { ContextGetterService } from '../context.getter.service';
+import { Injectable } from '@nestjs/common';
+import { NftToken } from 'src/modules/tokens/models/nftToken.model';
 
+@Injectable()
 export class ContextGetterServiceMock {
+    constructor(private readonly mxApi: MXApiService) {}
+
     async getCurrentEpoch(): Promise<number> {
         return 1;
     }
 
     async getShardCurrentBlockNonce(shardID: number): Promise<number> {
         return 111;
+    }
+
+    async getNftsCountForUser(address: string): Promise<number> {
+        return (await this.getNftsForUser(address)).length;
+    }
+
+    async getNftsForUser(
+        address: string,
+        from = 0,
+        size = 100,
+        type = 'MetaESDT',
+        collections?: string[],
+    ): Promise<NftToken[]> {
+        return await this.mxApi.getNftsForUser(address);
     }
 }
 
