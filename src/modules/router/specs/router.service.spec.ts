b6584e78719f206d1375c580f6b2bcc48d024700
hschiau <horia.schiau@multiversx.com>
2024-05-15 16:55:39 +0300

    SERVICES-2370: update router service unit tests

--- src/modules/router/specs/router.service.spec.ts
+++ src/modules/router/specs/router.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RouterService } from '../services/router.service';
-import { PairFilterArgs } from '../models/filter.args';
+import { PairFilterArgs, PairPaginationArgs } from '../models/filter.args';
 import { PairModel } from 'src/modules/pair/models/pair.model';
 import { PairAbiServiceProvider } from 'src/modules/pair/mocks/pair.abi.service.mock';
 import { RouterAbiServiceProvider } from '../mocks/router.abi.service.mock';
@@ -14,6 +14,7 @@ import { PairComputeServiceProvider } from 'src/modules/pair/mocks/pair.compute.
 
 describe('RouterService', () => {
     let module: TestingModule;
+    let defaultPagination: PairPaginationArgs;
 
     beforeAll(async () => {
         module = await Test.createTestingModule({
@@ -32,6 +33,11 @@ describe('RouterService', () => {
                 ApiConfigService,
             ],
         }).compile();
+
+        defaultPagination = new PairPaginationArgs({
+            offset: 0,
+            limit: Number.MAX_VALUE,
+        });
     });
 
     it('should be defined', () => {
@@ -43,8 +49,7 @@ describe('RouterService', () => {
         const service = module.get<RouterService>(RouterService);
 
         const allPairs = await service.getAllPairs(
-            0,
-            Number.MAX_VALUE,
+            defaultPagination,
             new PairFilterArgs(),
         );
         expect(allPairs).toEqual([
@@ -94,7 +99,7 @@ describe('RouterService', () => {
     it('should get filtered pairs', async () => {
         const service = module.get<RouterService>(RouterService);
 
-        const filteredPairs = await service.getAllPairs(0, Number.MAX_VALUE, {
+        const filteredPairs = await service.getAllPairs(defaultPagination, {
             firstTokenID: 'WEGLD-123456',
             issuedLpToken: true,
             address: null,
@@ -103,6 +108,10 @@ describe('RouterService', () => {
             feeState: null,
             minVolume: null,
             minLockedValueUSD: null,
+            minTradesCount: null,
+            hasFarms: null,
+            hasDualFarms: null,
+            minDeployedAt: null,
         });
         expect(filteredPairs).toEqual([
             new PairModel({
@@ -131,7 +140,7 @@ describe('RouterService', () => {
     it('should get pairs filtered by fee state and volume', async () => {
         const service = module.get<RouterService>(RouterService);
 
-        const filteredPairs = await service.getAllPairs(0, Number.MAX_VALUE, {
+        const filteredPairs = await service.getAllPairs(defaultPagination, {
             firstTokenID: 'WEGLD-123456',
             issuedLpToken: true,
             address: null,
@@ -140,6 +149,10 @@ describe('RouterService', () => {
             feeState: false,
             minVolume: 1000,
             minLockedValueUSD: null,
+            minTradesCount: null,
+            hasFarms: null,
+            hasDualFarms: null,
+            minDeployedAt: null,
         });
         expect(filteredPairs).toEqual([
             new PairModel({
@@ -153,7 +166,7 @@ describe('RouterService', () => {
     it('should get pairs filtered by minimum locked value USD', async () => {
         const service = module.get<RouterService>(RouterService);
 
-        const filteredPairs = await service.getAllPairs(0, Number.MAX_VALUE, {
+        const filteredPairs = await service.getAllPairs(defaultPagination, {
             firstTokenID: null,
             issuedLpToken: true,
             address: null,
@@ -162,6 +175,10 @@ describe('RouterService', () => {
             feeState: null,
             minVolume: null,
             minLockedValueUSD: 300,
+            minTradesCount: null,
+            hasFarms: null,
+            hasDualFarms: null,
+            minDeployedAt: null,
         });
         expect(filteredPairs).toEqual([
             new PairModel({
@@ -197,6 +214,63 @@ describe('RouterService', () => {
             new PairModel({
                 address: Address.fromHex(
                     '0000000000000000000000000000000000000000000000000000000000000019',
+                ).bech32(),
+            }),
+        ]);
+    });
+
+    it('should get pairs filtered by minimum trades count', async () => {
+        const service = module.get<RouterService>(RouterService);
+
+        const filteredPairs = await service.getAllPairs(defaultPagination, {
+            firstTokenID: null,
+            issuedLpToken: true,
+            address: null,
+            secondTokenID: null,
+            state: null,
+            feeState: null,
+            minVolume: null,
+            minLockedValueUSD: null,
+            minTradesCount: 300,
+            hasFarms: null,
+            hasDualFarms: null,
+            minDeployedAt: null,
+        });
+        expect(filteredPairs).toEqual([
+            new PairModel({
+                address: Address.fromHex(
+                    '0000000000000000000000000000000000000000000000000000000000000012',
+                ).bech32(),
+            }),
+            new PairModel({
+                address: Address.fromHex(
+                    '0000000000000000000000000000000000000000000000000000000000000013',
+                ).bech32(),
+            }),
+        ]);
+    });
+
+    it('should get pairs filtered by hasFarms field and minimum trades count', async () => {
+        const service = module.get<RouterService>(RouterService);
+
+        const filteredPairs = await service.getAllPairs(defaultPagination, {
+            firstTokenID: null,
+            issuedLpToken: true,
+            address: null,
+            secondTokenID: null,
+            state: null,
+            feeState: null,
+            minVolume: null,
+            minLockedValueUSD: null,
+            minTradesCount: 300,
+            hasFarms: true,
+            hasDualFarms: null,
+            minDeployedAt: null,
+        });
+        expect(filteredPairs).toEqual([
+            new PairModel({
+                address: Address.fromHex(
+                    '0000000000000000000000000000000000000000000000000000000000000013',
                 ).bech32(),
             }),
         ]);
