75bdc9d71f8587400ea986f72451404e5c96c72a
hschiau <horia.schiau@multiversx.com>
2024-06-28 09:24:07 +0300

    SERVICES-2474: fix unit tests imports

--- src/modules/staking-proxy/staking.proxy.module.ts
+++ src/modules/staking-proxy/staking.proxy.module.ts
@@ -37,6 +37,7 @@ import { StakingProxyFilteringService } from './services/staking.proxy.filtering
         StakingProxyAbiService,
         StakingProxyService,
         StakingProxySetterService,
+        StakingProxyFilteringService,
     ],
 })
 export class StakingProxyModule {}
