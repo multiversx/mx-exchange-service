6167fb120bb7db43ce48dbb12f014c5ee4e03685
hschiau <horia.schiau@multiversx.com>
2024-06-28 16:36:04 +0300

    SERVICES-2474: add all types of tokens to filtering

--- src/modules/staking-proxy/services/staking.proxy.filtering.service.ts
+++ src/modules/staking-proxy/services/staking.proxy.filtering.service.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable, forwardRef } from '@nestjs/common';
 import { StakingProxiesFilter } from '../models/staking.proxy.args.model';
 import { StakingProxyAbiService } from './staking.proxy.abi.service';
 import { StakingProxyService } from './staking.proxy.service';
+import { EsdtToken } from 'src/modules/tokens/models/esdtToken.model';
+import { NftCollection } from 'src/modules/tokens/models/nftCollection.model';
 
 @Injectable()
 export class StakingProxyFilteringService {
@@ -97,19 +99,55 @@ export class StakingProxyFilteringService {
             ),
         );
 
+        const farmTokens = await Promise.all(
+            stakingProxyAddresses.map((address) =>
+                this.stakingProxyService.getFarmToken(address),
+            ),
+        );
+
+        const dualYieldTokens = await Promise.all(
+            stakingProxyAddresses.map((address) =>
+                this.stakingProxyService.getDualYieldToken(address),
+            ),
+        );
+
+        const lpFarmTokens = await Promise.all(
+            stakingProxyAddresses.map((address) =>
+                this.stakingProxyService.getLpFarmToken(address),
+            ),
+        );
+
         const filteredAddresses: string[] = [];
         for (const [index, address] of stakingProxyAddresses.entries()) {
             const stakingToken = stakingTokens[index];
+            const farmToken = farmTokens[index];
+            const dualYieldToken = dualYieldTokens[index];
+            const lpFarmToken = lpFarmTokens[index];
 
             if (
-                stakingToken.name.toUpperCase().includes(searchTerm) ||
-                stakingToken.identifier.toUpperCase().includes(searchTerm) ||
-                stakingToken.ticker.toUpperCase().includes(searchTerm)
+                this.tokenIncludesTerm(stakingToken, searchTerm) ||
+                this.tokenIncludesTerm(farmToken, searchTerm) ||
+                this.tokenIncludesTerm(dualYieldToken, searchTerm) ||
+                this.tokenIncludesTerm(lpFarmToken, searchTerm)
             ) {
                 filteredAddresses.push(address);
             }
         }
 
         return filteredAddresses;
+    }
+
+    private tokenIncludesTerm(
+        token: EsdtToken | NftCollection,
+        term: string,
+    ): boolean {
+        const identifier =
+            'identifier' in token ? token.identifier : token.collection;
+
+        return (
+            token.name.toUpperCase().includes(term) ||
+            identifier.toUpperCase().includes(term) ||
+            token.ticker.toUpperCase().includes(term)
+        );
     }
 }
