04002fdda246331c03f0352e968408380ff25fc4
hschiau <horia.schiau@multiversx.com>
2024-07-18 11:34:23 +0300

    SERVICES-2498: add comments describing BarResponse fields

--- src/modules/trading-view/dtos/bars.response.ts
+++ src/modules/trading-view/dtos/bars.response.ts
@@ -27,14 +27,23 @@ export enum TradingViewResolution {
 }
 
 export class BarsResponse {
+    // status code. Expected values: ok | error | no_data
     s: string;
+    // Error message. Should be present only when s = 'error'
     errmsg?: string;
+    // Bar time. Unix timestamp (UTC)
     t: number[];
+    // Opening price
     o: number[];
+    // High price
     h: number[];
+    // Low price
     l: number[];
+    // Closing price
     c: number[];
+    // Volume
     v?: number[];
+    // Time of the next bar if there is no data (status code is no_data) in the requested period
     nextTime?: number;
 
     constructor(init?: Partial<BarsResponse>) {
