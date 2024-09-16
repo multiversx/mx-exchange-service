0a6500dd4243557cc5366f78eaadf515ef7a32c2
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2024-09-16 18:49:23 +0300

    MEX-514: fix compute user APR if no user total farm amount
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/farm/v2/services/farm.v2.compute.service.ts
+++ src/modules/farm/v2/services/farm.v2.compute.service.ts
@@ -327,6 +327,10 @@ export class FarmComputeServiceV2
             .plus(additionalUserFarmAmount)
             .toFixed();
 
+        if (userTotalFarmPosition === '0') {
+            return 0;
+        }
+
         const userRewardsPerWeek = await this.computeUserRewardsForWeek(
             scAddress,
             userAddress,
@@ -384,6 +388,10 @@ export class FarmComputeServiceV2
         userTotalFarmPosition = new BigNumber(userTotalFarmPosition)
             .plus(additionalUserFarmAmount)
             .toFixed();
+
+        if (userTotalFarmPosition === '0') {
+            return 0;
+        }
 
         const userMaxRewardsPerWeek = new BigNumber(
             boostedRewardsPerWeek[0].amount,
