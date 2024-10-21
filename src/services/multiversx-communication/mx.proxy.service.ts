dd78ba2251f72a17cd25dd9820337ae0d8f862dd
hschiau <horia.schiau@multiversx.com>
2024-10-21 15:26:50 +0300

    SERVICES-2660: add clientName to apiNetworkProvider

--- src/services/multiversx-communication/mx.proxy.service.ts
+++ src/services/multiversx-communication/mx.proxy.service.ts
@@ -49,7 +49,7 @@ export class MXProxyService {
                 headers: {
                     origin: 'xExchangeService',
                 },
-                clientName: 'xexchange-api',
+                clientName: 'xExchangeService',
             },
         );
 
