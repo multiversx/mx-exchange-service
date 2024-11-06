be7611df868fb57b097adc7183ec20f505dba141
hschiau <horia.schiau@multiversx.com>
2024-11-06 14:53:37 +0200

    add hostname to init call

--- src/tracer.ts
+++ src/tracer.ts
@@ -4,11 +4,13 @@ import { envload } from './config/env_load';
 envload();
 
 if (process.env.ENABLE_TRACER === 'true') {
+    console.log('DD_TRACE_AGENT_HOSTNAME', process.env.DD_TRACE_AGENT_HOSTNAME);
     tracer.init({
         logInjection: true,
         env: process.env.NODE_ENV,
         profiling: true,
         service: 'xechange-graph',
+        hostname: process.env.DD_TRACE_AGENT_HOSTNAME ?? 'localhost',
     });
 
     tracer.use('http', {
