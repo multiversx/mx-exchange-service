f5fc511bf545ea1102ebd58dbbfbf69b4a80ab82
hschiau <horia.schiau@multiversx.com>
2024-12-11 19:43:21 +0200

    MEX-527: add pairs memory store to factory service

--- src/modules/memory-store/services/memory.store.factory.service.ts
+++ src/modules/memory-store/services/memory.store.factory.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { IMemoryStoreService } from './interfaces';
+import { PairMemoryStoreService } from './pair.memory.store.service';
+import { PairModel } from 'src/modules/pair/models/pair.model';
+import { PairsResponse } from 'src/modules/pair/models/pairs.response';
 
 @Injectable()
 export class MemoryStoreFactoryService {
     private queryMapping: Record<string, IMemoryStoreService<any, any>> = {};
 
+    constructor(private readonly pairMemoryStore: PairMemoryStoreService) {
+        const pairQueries = Object.keys(
+            this.pairMemoryStore.getTargetedQueries(),
+        );
+
+        for (const query of pairQueries) {
+            this.queryMapping[query] = this
+                .pairMemoryStore as IMemoryStoreService<
+                PairModel[],
+                PairsResponse
+            >;
+        }
+    }
+
     isReady(): boolean {
-        // TODO: replace with actual checks on compatible memory store services
-        return false;
+        return this.pairMemoryStore.isReady();
     }
 
     getTargetedQueryNames(): string[] {
