6435d18e31868e80fb06f4da73a5b9c258723351
hschiau <horia.schiau@multiversx.com>
2024-12-11 19:20:01 +0200

    MEX-527: add cron jobs that initialize and maintain memory store data

---
+++ src/modules/memory-store/memory.store.cron.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { PairModule } from '../pair/pair.module';
+import { RouterModule } from '../router/router.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { StakingProxyModule } from '../staking-proxy/staking.proxy.module';
+import { EnergyModule } from '../energy/energy.module';
+import { FarmModuleV2 } from '../farm/v2/farm.v2.module';
+import { StakingModule } from '../staking/staking.module';
+import { TokenModule } from '../tokens/token.module';
+import { FarmModule } from '../farm/farm.module';
+import { MemoryStoreCronService } from './services/memory.store.cron.service';
+
+@Module({
+    imports: [
+        EnergyModule,
+        FarmModule,
+        FarmModuleV2,
+        PairModule,
+        RouterModule,
+        ScheduleModule.forRoot(),
+        StakingModule,
+        StakingProxyModule,
+        TokenModule,
+    ],
+    providers: [MemoryStoreCronService],
+    exports: [],
+})
+export class MemoryStoreCronModule {}
