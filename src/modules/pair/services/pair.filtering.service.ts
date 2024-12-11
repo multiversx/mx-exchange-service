637d23ef6255b1de81664bef8290b58701492c46
hschiau <horia.schiau@multiversx.com>
2024-12-11 17:52:58 +0200

    MEX-527: refactor pair filtering service
    - update PairFilteringService to work with pair models with the fields already populated (functional service)
    - update PairMetadataBuilder to use the functional filtering service
    - update pair methods (regular and filtered) in router service to use the builder
    - remove duplicate code in router service

--- src/modules/pair/services/pair.filtering.service.ts
+++ src/modules/pair/services/pair.filtering.service.ts
@@ -1,334 +1,247 @@
-import { Injectable } from '@nestjs/common';
-import { PairAbiService } from './pair.abi.service';
-import { PairComputeService } from './pair.compute.service';
-import { PairMetadata } from 'src/modules/router/models/pair.metadata.model';
 import {
     PairFilterArgs,
     PairsFilter,
 } from 'src/modules/router/models/filter.args';
 import BigNumber from 'bignumber.js';
-import { PairService } from './pair.service';
+import { PairModel } from '../models/pair.model';
 
-@Injectable()
 export class PairFilteringService {
-    constructor(
-        private readonly pairAbi: PairAbiService,
-        private readonly pairCompute: PairComputeService,
-        private readonly pairService: PairService,
-    ) {}
-
-    async pairsByIssuedLpToken(
-        pairFilter: PairFilterArgs | PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (!pairFilter.issuedLpToken) {
-            return pairsMetadata;
+    static pairsByIssuedLpToken(
+        filters: PairFilterArgs | PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (!filters.issuedLpToken) {
+            return pairs;
         }
 
-        const lpTokensIDs = await this.pairService.getAllLpTokensIds(
-            pairsMetadata.map((pairMetadata) => pairMetadata.address),
-        );
-
-        const filteredPairsMetadata = [];
-        for (let index = 0; index < lpTokensIDs.length; index++) {
-            if (
-                lpTokensIDs[index] === undefined ||
-                lpTokensIDs[index] === 'undefined' ||
-                lpTokensIDs[index] === ''
-            ) {
-                continue;
-            }
-            filteredPairsMetadata.push(pairsMetadata[index]);
-        }
-
-        return filteredPairsMetadata;
+        return pairs.filter((pair) => pair.liquidityPoolToken !== undefined);
     }
 
-    async pairsByAddress(
-        pairFilter: PairFilterArgs | PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (pairFilter.addresses) {
-            pairsMetadata = pairsMetadata.filter((pair) =>
-                pairFilter.addresses.includes(pair.address),
-            );
+    static pairsByAddress(
+        filters: PairFilterArgs | PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (!filters.addresses || filters.addresses.length === 0) {
+            return pairs;
         }
-        return await Promise.resolve(pairsMetadata);
+
+        return pairs.filter((pair) => filters.addresses.includes(pair.address));
     }
 
-    async pairsByTokens(
-        pairFilter: PairFilterArgs | PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (pairFilter.firstTokenID) {
-            if (pairFilter.secondTokenID) {
-                pairsMetadata = pairsMetadata.filter(
+    static pairsByTokens(
+        filters: PairFilterArgs | PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (filters instanceof PairsFilter) {
+            pairs = PairFilteringService.pairsByWildcardToken(filters, pairs);
+        }
+
+        if (filters.firstTokenID) {
+            if (filters.secondTokenID) {
+                pairs = pairs.filter(
                     (pair) =>
-                        (pairFilter.firstTokenID === pair.firstTokenID &&
-                            pairFilter.secondTokenID === pair.secondTokenID) ||
-                        (pairFilter.firstTokenID === pair.secondTokenID &&
-                            pairFilter.secondTokenID === pair.firstTokenID),
+                        (pair.firstToken.identifier === filters.firstTokenID &&
+                            pair.secondToken.identifier ===
+                                filters.secondTokenID) ||
+                        (pair.firstToken.identifier === filters.secondTokenID &&
+                            pair.secondToken.identifier ===
+                                filters.firstTokenID),
                 );
             } else {
-                pairsMetadata = pairsMetadata.filter(
-                    (pair) => pairFilter.firstTokenID === pair.firstTokenID,
+                pairs = pairs.filter(
+                    (pair) =>
+                        pair.firstToken.identifier === filters.firstTokenID,
                 );
             }
-        } else if (pairFilter.secondTokenID) {
-            pairsMetadata = pairsMetadata.filter(
-                (pair) => pairFilter.secondTokenID === pair.secondTokenID,
+        } else if (filters.secondTokenID) {
+            pairs = pairs.filter(
+                (pair) => pair.secondToken.identifier === filters.secondTokenID,
             );
         }
-        return await Promise.resolve(pairsMetadata);
+
+        return pairs;
     }
 
-    async pairsByWildcardToken(
-        pairFilter: PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (
-            !pairFilter.searchToken ||
-            pairFilter.searchToken.trim().length < 3
-        ) {
-            return pairsMetadata;
+    static pairsByWildcardToken(
+        filters: PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (!filters.searchToken || filters.searchToken.trim().length < 3) {
+            return pairs;
         }
 
-        const searchTerm = pairFilter.searchToken.toUpperCase().trim();
-        const pairsAddresses = pairsMetadata.map(
-            (pairMetadata) => pairMetadata.address,
-        );
+        const searchTerm = filters.searchToken.toUpperCase().trim();
 
-        const pairsFirstToken = await this.pairService.getAllFirstTokens(
-            pairsAddresses,
-        );
-        const pairsSecondToken = await this.pairService.getAllSecondTokens(
-            pairsAddresses,
-        );
-
-        const filteredPairs: PairMetadata[] = [];
-        for (const [index, pair] of pairsMetadata.entries()) {
-            const firstToken = pairsFirstToken[index];
-            const secondToken = pairsSecondToken[index];
-
-            if (
-                firstToken.name.toUpperCase().includes(searchTerm) ||
-                firstToken.identifier.toUpperCase().includes(searchTerm) ||
-                firstToken.ticker.toUpperCase().includes(searchTerm) ||
-                secondToken.name.toUpperCase().includes(searchTerm) ||
-                secondToken.identifier.toUpperCase().includes(searchTerm) ||
-                secondToken.ticker.toUpperCase().includes(searchTerm)
-            ) {
-                filteredPairs.push(pair);
-            }
-        }
-
-        return filteredPairs;
-    }
-
-    async pairsByLpTokenIds(
-        pairFilter: PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (!pairFilter.lpTokenIds || pairFilter.lpTokenIds.length === 0) {
-            return pairsMetadata;
-        }
-
-        const lpTokensIDs = await this.pairService.getAllLpTokensIds(
-            pairsMetadata.map((pairMetadata) => pairMetadata.address),
-        );
-
-        return pairsMetadata.filter((_, index) =>
-            pairFilter.lpTokenIds.includes(lpTokensIDs[index]),
+        return pairs.filter(
+            (pair) =>
+                pair.firstToken.name.toUpperCase().includes(searchTerm) ||
+                pair.firstToken.identifier.toUpperCase().includes(searchTerm) ||
+                pair.firstToken.ticker.toUpperCase().includes(searchTerm) ||
+                pair.secondToken.name.toUpperCase().includes(searchTerm) ||
+                pair.secondToken.identifier
+                    .toUpperCase()
+                    .includes(searchTerm) ||
+                pair.secondToken.ticker.toUpperCase().includes(searchTerm),
         );
     }
 
-    async pairsByFarmTokens(
-        pairFilter: PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (!pairFilter.farmTokens || pairFilter.farmTokens.length === 0) {
-            return pairsMetadata;
+    static pairsByLpTokenIds(
+        filters: PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (!filters.lpTokenIds || filters.lpTokenIds.length === 0) {
+            return pairs;
         }
 
-        const farmTokens = await Promise.all(
-            pairsMetadata.map((pairMetadata) =>
-                this.pairCompute.getPairFarmToken(pairMetadata.address),
-            ),
+        return pairs.filter(
+            (pair) =>
+                pair.liquidityPoolToken !== undefined &&
+                filters.lpTokenIds.includes(pair.liquidityPoolToken.identifier),
         );
+    }
 
-        return pairsMetadata.filter(
+    static pairsByFarmTokens(
+        filters: PairsFilter,
+        pairs: PairModel[],
+        farmTokens: string[],
+    ): PairModel[] {
+        if (!filters.farmTokens || filters.farmTokens.length === 0) {
+            return pairs;
+        }
+
+        return pairs.filter(
             (_, index) =>
                 farmTokens[index] &&
-                pairFilter.farmTokens.includes(farmTokens[index]),
+                filters.farmTokens.includes(farmTokens[index]),
         );
     }
 
-    async pairsByState(
-        pairFilter: PairFilterArgs | PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (!pairFilter.state || pairFilter.state.length === 0) {
-            return pairsMetadata;
+    static pairsByState(
+        filters: PairFilterArgs | PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (
+            !filters.state ||
+            (Array.isArray(filters.state) && filters.state.length === 0)
+        ) {
+            return pairs;
         }
 
-        const pairsStates = await this.pairService.getAllStates(
-            pairsMetadata.map((pair) => pair.address),
-        );
-
-        return pairsMetadata.filter((_, index) => {
-            if (!Array.isArray(pairFilter.state)) {
-                return pairsStates[index] === pairFilter.state;
+        return pairs.filter((pair) => {
+            if (!Array.isArray(filters.state)) {
+                return pair.state === filters.state;
             }
 
-            return pairFilter.state.includes(pairsStates[index]);
+            return filters.state.includes(pair.state);
         });
     }
 
-    async pairsByFeeState(
-        pairFilter: PairFilterArgs | PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
+    static pairsByFeeState(
+        filters: PairFilterArgs | PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
         if (
-            typeof pairFilter.feeState === 'undefined' ||
-            pairFilter.feeState === null
+            typeof filters.feeState === 'undefined' ||
+            filters.feeState === null
         ) {
-            return pairsMetadata;
+            return pairs;
         }
 
-        const pairsFeeStates = await this.pairService.getAllFeeStates(
-            pairsMetadata.map((pair) => pair.address),
-        );
-
-        return pairsMetadata.filter(
-            (_, index) => pairsFeeStates[index] === pairFilter.feeState,
-        );
+        return pairs.filter((pair) => pair.feeState === filters.feeState);
     }
 
-    async pairsByVolume(
-        pairFilter: PairFilterArgs | PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (!pairFilter.minVolume) {
-            return pairsMetadata;
+    static pairsByVolume(
+        filters: PairFilterArgs | PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (!filters.minVolume) {
+            return pairs;
         }
 
-        const pairsVolumes = await this.pairCompute.getAllVolumeUSD(
-            pairsMetadata.map((pair) => pair.address),
-        );
-
-        return pairsMetadata.filter((_, index) => {
-            const volume = new BigNumber(pairsVolumes[index]);
-            return volume.gte(pairFilter.minVolume);
+        return pairs.filter((pair) => {
+            return new BigNumber(pair.volumeUSD24h).gte(filters.minVolume);
         });
     }
 
-    async pairsByLockedValueUSD(
-        pairFilter: PairFilterArgs | PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (!pairFilter.minLockedValueUSD) {
-            return pairsMetadata;
+    static pairsByLockedValueUSD(
+        filters: PairFilterArgs | PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (!filters.minLockedValueUSD) {
+            return pairs;
         }
 
-        const pairsLiquidityUSD = await this.pairService.getAllLockedValueUSD(
-            pairsMetadata.map((pair) => pair.address),
-        );
-
-        return pairsMetadata.filter((_, index) => {
-            const lockedValueUSD = new BigNumber(pairsLiquidityUSD[index]);
-            return lockedValueUSD.gte(pairFilter.minLockedValueUSD);
+        return pairs.filter((pair) => {
+            return new BigNumber(pair.lockedValueUSD).gte(
+                filters.minLockedValueUSD,
+            );
         });
     }
 
-    async pairsByTradesCount(
-        pairFilter: PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (!pairFilter.minTradesCount) {
-            return pairsMetadata;
+    static pairsByTradesCount(
+        filters: PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (!filters.minTradesCount) {
+            return pairs;
         }
 
-        const pairsTradesCount = await this.pairService.getAllTradesCount(
-            pairsMetadata.map((pair) => pair.address),
-        );
-
-        return pairsMetadata.filter(
-            (_, index) => pairsTradesCount[index] >= pairFilter.minTradesCount,
+        return pairs.filter(
+            (pair) => pair.tradesCount >= filters.minTradesCount,
         );
     }
 
-    async pairsByTradesCount24h(
-        pairFilter: PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (!pairFilter.minTradesCount24h) {
-            return pairsMetadata;
+    static pairsByTradesCount24h(
+        filters: PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (!filters.minTradesCount24h) {
+            return pairs;
         }
 
-        const pairsTradesCount24h = await this.pairCompute.getAllTradesCount24h(
-            pairsMetadata.map((pair) => pair.address),
-        );
-
-        return pairsMetadata.filter(
-            (_, index) =>
-                pairsTradesCount24h[index] >= pairFilter.minTradesCount24h,
+        return pairs.filter(
+            (pair) => pair.tradesCount24h >= filters.minTradesCount24h,
         );
     }
 
-    async pairsByHasFarms(
-        pairFilter: PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
+    static pairsByHasFarms(
+        filters: PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
         if (
-            typeof pairFilter.hasFarms === 'undefined' ||
-            pairFilter.hasFarms === null
+            typeof filters.hasFarms === 'undefined' ||
+            filters.hasFarms === null
         ) {
-            return pairsMetadata;
+            return pairs;
         }
 
-        const pairsHasFarms = await this.pairService.getAllHasFarms(
-            pairsMetadata.map((pair) => pair.address),
-        );
-
-        return pairsMetadata.filter(
-            (_, index) => pairsHasFarms[index] === pairFilter.hasFarms,
-        );
+        return pairs.filter((pair) => pair.hasFarms === filters.hasFarms);
     }
 
-    async pairsByHasDualFarms(
-        pairFilter: PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
+    static pairsByHasDualFarms(
+        filters: PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
         if (
-            typeof pairFilter.hasDualFarms === 'undefined' ||
-            pairFilter.hasDualFarms === null
+            typeof filters.hasDualFarms === 'undefined' ||
+            filters.hasDualFarms === null
         ) {
-            return pairsMetadata;
+            return pairs;
         }
 
-        const pairsHasDualFarms = await this.pairService.getAllHasDualFarms(
-            pairsMetadata.map((pair) => pair.address),
-        );
-
-        return pairsMetadata.filter(
-            (_, index) => pairsHasDualFarms[index] === pairFilter.hasDualFarms,
+        return pairs.filter(
+            (pair) => pair.hasDualFarms === filters.hasDualFarms,
         );
     }
 
-    async pairsByDeployedAt(
-        pairFilter: PairsFilter,
-        pairsMetadata: PairMetadata[],
-    ): Promise<PairMetadata[]> {
-        if (!pairFilter.minDeployedAt) {
-            return pairsMetadata;
+    static pairsByDeployedAt(
+        filters: PairsFilter,
+        pairs: PairModel[],
+    ): PairModel[] {
+        if (!filters.minDeployedAt) {
+            return pairs;
         }
 
-        const pairsDeployedAt = await this.pairService.getAllDeployedAt(
-            pairsMetadata.map((pair) => pair.address),
-        );
-
-        return pairsMetadata.filter(
-            (_, index) => pairsDeployedAt[index] >= pairFilter.minDeployedAt,
-        );
+        return pairs.filter((pair) => pair.deployedAt >= filters.minDeployedAt);
     }
 }
