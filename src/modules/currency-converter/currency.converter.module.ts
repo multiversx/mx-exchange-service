a28c1cd7cb4c26b2d2acae80c0ea6b79cbd80b04
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2025-02-19 18:54:34 +0200

    MEX-666: remove unused import
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/currency-converter/currency.converter.module.ts
+++ src/modules/currency-converter/currency.converter.module.ts
@@ -4,18 +4,14 @@ import { CurrencyConverterService } from './services/currency.converter.service'
 import { CurrencyConverterSetterService } from './services/currency.converter.setter.service';
 import { ApiConfigService } from 'src/helpers/api.config.service';
 import { TokenModule } from '../tokens/token.module';
-import { MXDataApiService } from 'src/services/multiversx-communication/mx.data.api.service';
 
 @Module({
-    imports: [
-        TokenModule,
-    ],
+    imports: [TokenModule],
     providers: [
         ApiConfigService,
         CurrencyConverterResolver,
         CurrencyConverterService,
         CurrencyConverterSetterService,
-        MXDataApiService,
     ],
     exports: [CurrencyConverterService, CurrencyConverterSetterService],
 })
