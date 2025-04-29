3a7eab65d2f9254bd4f68ffd38e999eb7cc13957
hschiau <horia.schiau@multiversx.com>
2025-04-29 17:10:45 +0300

    MEX-766: smart router evaluation REST endpoints
    - add private app controller that exposes endpoints needed to populate dashboard
    - add service methods for querying mongodb (with pagination, filtering and sorting)

---
+++ src/modules/smart-router-evaluation/smart.router.evaluation.controller.ts
@@ -0,0 +1,44 @@
+import {
+    Controller,
+    Get,
+    Query,
+    UseGuards,
+    ValidationPipe,
+} from '@nestjs/common';
+import { SmartRouterEvaluationService } from './services/smart.router.evaluation.service';
+import { SwapRoute } from './schemas/swap.route.schema';
+import { SwapsEvaluationParams } from './dtos/swaps.evaluation.params';
+import { BaseEsdtToken } from '../tokens/models/esdtToken.model';
+import { JwtOrNativeAdminGuard } from '../auth/jwt.or.native.admin.guard';
+
+@Controller('smart-router')
+export class SmartRouterEvaluationController {
+    constructor(
+        private readonly smartRouterEvaluationService: SmartRouterEvaluationService,
+    ) {}
+
+    @UseGuards(JwtOrNativeAdminGuard)
+    @Get('/swaps')
+    async getComparisonSwapRoutes(
+        @Query(
+            new ValidationPipe({
+                transform: true,
+                transformOptions: { enableImplicitConversion: true },
+            }),
+        )
+        params: SwapsEvaluationParams,
+    ): Promise<{ totalCount: number; swaps: SwapRoute[] }> {
+        return this.smartRouterEvaluationService.getComparisonSwapRoutes(
+            params,
+        );
+    }
+
+    @UseGuards(JwtOrNativeAdminGuard)
+    @Get('/swaps/tokens')
+    async getDistinctTokens(): Promise<{
+        tokensIn: BaseEsdtToken[];
+        tokensOut: BaseEsdtToken[];
+    }> {
+        return this.smartRouterEvaluationService.getDistinctTokens();
+    }
+}
--- src/modules/smart-router-evaluation/smart.router.evaluation.module.ts
+++ src/modules/smart-router-evaluation/smart.router.evaluation.module.ts
@@ -3,14 +3,23 @@ import { SmartRouterEvaluationService } from './services/smart.router.evaluation
 import { MongooseModule } from '@nestjs/mongoose';
 import { SwapRoute, SwapRouteSchema } from './schemas/swap.route.schema';
 import { SwapRouteRepositoryService } from 'src/services/database/repositories/swap.route.repository';
+import { TokenModule } from '../tokens/token.module';
+import { ApiConfigService } from 'src/helpers/api.config.service';
+import { SmartRouterEvaluationController } from './smart.router.evaluation.controller';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: SwapRoute.name, schema: SwapRouteSchema },
         ]),
+        TokenModule,
     ],
-    providers: [SmartRouterEvaluationService, SwapRouteRepositoryService],
+    providers: [
+        SmartRouterEvaluationService,
+        SwapRouteRepositoryService,
+        SmartRouterEvaluationController,
+        ApiConfigService,
+    ],
     exports: [SmartRouterEvaluationService],
 })
 export class SmartRouterEvaluationModule {}
