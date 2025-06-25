3848c0cf9ce661ec33528c0559bf8ac28c987ce7
hschiau <horia.schiau@multiversx.com>
2025-06-25 16:59:03 +0300

    MEX-775: add composable tasks abi to module providers and exports arrays

--- src/modules/composable-tasks/composable.tasks.module.ts
+++ src/modules/composable-tasks/composable.tasks.module.ts
@@ -3,10 +3,15 @@ import { MXCommunicationModule } from 'src/services/multiversx-communication/mx.
 import { ComposableTasksTransactionService } from './services/composable.tasks.transaction';
 import { ComposableTasksResolver } from './composable.tasks.resolver';
 import { WrappingModule } from '../wrapping/wrap.module';
+import { ComposableTasksAbiService } from './services/composable.tasks.abi.service';
 
 @Module({
     imports: [MXCommunicationModule, WrappingModule],
-    providers: [ComposableTasksTransactionService, ComposableTasksResolver],
-    exports: [ComposableTasksTransactionService],
+    providers: [
+        ComposableTasksTransactionService,
+        ComposableTasksResolver,
+        ComposableTasksAbiService,
+    ],
+    exports: [ComposableTasksTransactionService, ComposableTasksAbiService],
 })
 export class ComposableTasksModule {}
