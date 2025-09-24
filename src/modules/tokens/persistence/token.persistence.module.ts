0e2960ccd93726d23d3981695733d7bdbea5177f
hschiau <horia.schiau@multiversx.com>
2025-09-24 11:02:44 +0300

    refactor token persistence service

--- src/modules/tokens/persistence/token.persistence.module.ts
+++ src/modules/tokens/persistence/token.persistence.module.ts
@@ -1,11 +1,10 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EsdtToken } from '../models/esdtToken.model';
 import { TokenRepository } from './services/token.repository';
 import { TokenPersistenceService } from './services/token.persistence.service';
 import { TokenModule } from '../token.module';
 import { EsdtTokenSchema } from './schemas/esdtToken.schema';
-import { PairPersistenceModule } from 'src/modules/pair/persistence/pair.persistence.module';
 
 @Module({
     imports: [
@@ -13,7 +12,6 @@ import { PairPersistenceModule } from 'src/modules/pair/persistence/pair.persist
             { name: EsdtToken.name, schema: EsdtTokenSchema },
         ]),
         TokenModule,
-        forwardRef(() => PairPersistenceModule),
     ],
     providers: [TokenRepository, TokenPersistenceService],
     exports: [TokenPersistenceService],
