eba37c3cec63d8ae38564c900503e9a562d1f230
hschiau <horia.schiau@multiversx.com>
2025-10-13 19:08:11 +0300

    MEX-910: added Pair schema
    - decorated Pair fields
    - added PairRepository

--- src/modules/persistence/persistence.module.ts
+++ src/modules/persistence/persistence.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PairModel } from '../pair/models/pair.model';
 import { EsdtToken } from '../tokens/models/esdtToken.model';
+import { PairRepository } from './repositories/pair.repository';
 import { TokenRepository } from './repositories/token.repository';
 import { EsdtTokenSchema } from './schemas/esdtToken.schema';
+import { PairSchema } from './schemas/pair.schema';
 import { TokenPersistenceService } from './services/token.persistence.service';
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: EsdtToken.name, schema: EsdtTokenSchema },
+            { name: PairModel.name, schema: PairSchema },
         ]),
     ],
-    providers: [TokenRepository, TokenPersistenceService],
+    providers: [TokenRepository, TokenPersistenceService, PairRepository],
     exports: [TokenPersistenceService],
     controllers: [],
 })
