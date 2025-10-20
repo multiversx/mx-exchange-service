75731917f88ea16baa02ebdc4b10d396ac894f4b
hschiau <horia.schiau@multiversx.com>
2025-10-20 17:35:02 +0300

    MEX-914: compute price directly from reserves

--- src/modules/aggregators/services/mex.service.ts
+++ src/modules/aggregators/services/mex.service.ts
@@ -1,19 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import BigNumber from 'bignumber.js';
+import { scAddress, tokenProviderUSD } from 'src/config';
 import { EnergyAbiService } from 'src/modules/energy/services/energy.abi.service';
-import { TokenComputeService } from 'src/modules/tokens/services/token.compute.service';
+import { PairComputeService } from 'src/modules/pair/services/pair.compute.service';
+import { RouterAbiService } from 'src/modules/router/services/router.abi.service';
 
 @Injectable()
 export class MexService {
     constructor(
         private readonly energyAbi: EnergyAbiService,
-        private readonly tokenCompute: TokenComputeService,
+        private readonly pairCompute: PairComputeService,
+        private readonly routerAbi: RouterAbiService,
     ) {}
 
     async getPrice(): Promise<BigNumber> {
-        const tokenID = await this.energyAbi.baseAssetTokenID();
-        const price = await this.tokenCompute.tokenPriceDerivedUSD(tokenID);
+        const [mexTokenID, pairsMetadata, egldPriceInUSD] = await Promise.all([
+            this.energyAbi.baseAssetTokenID(),
+            this.routerAbi.pairsMetadata(),
+            this.pairCompute.firstTokenPrice(scAddress.WEGLD_USDC),
+        ]);
 
-        return new BigNumber(price);
+        const mexEgldPair = pairsMetadata.find((metadata) => {
+            return (
+                (metadata.firstTokenID === mexTokenID &&
+                    metadata.secondTokenID === tokenProviderUSD) ||
+                (metadata.firstTokenID === tokenProviderUSD &&
+                    metadata.secondTokenID === mexTokenID)
+            );
+        });
+
+        if (!mexEgldPair) {
+            throw new Error('MEX/WEGLD pair not found');
+        }
+
+        const mexPriceInEGLD =
+            mexEgldPair.firstTokenID === mexTokenID
+                ? await this.pairCompute.firstTokenPrice(mexEgldPair.address)
+                : await this.pairCompute.secondTokenPrice(mexEgldPair.address);
+
+        return new BigNumber(mexPriceInEGLD).times(egldPriceInUSD);
     }
 }
