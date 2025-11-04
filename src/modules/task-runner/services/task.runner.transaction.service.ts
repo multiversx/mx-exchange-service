ce5653611cfb3452a3588f934c6cb25139a3904e
hschiau <horia.schiau@multiversx.com>
2025-11-04 17:32:03 +0200

    MEX-926: extract tx signing and broadcasting in a separate service

---
+++ src/modules/task-runner/services/task.runner.transaction.service.ts
@@ -0,0 +1,128 @@
+import {
+    Transaction,
+    TransactionComputer,
+    TransactionWatcher,
+    UserSigner,
+} from '@multiversx/sdk-core';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
+import { promises } from 'fs';
+import { MXProxyService } from 'src/services/multiversx-communication/mx.proxy.service';
+import { ApiConfigService } from 'src/helpers/api.config.service';
+import { TransactionModel } from 'src/models/transaction.model';
+import { BroadcastStatus } from '../constants';
+import { MXApiService } from 'src/services/multiversx-communication/mx.api.service';
+
+@Injectable()
+export class TaskRunnerTransactionService implements OnModuleInit {
+    private accountSigner: UserSigner;
+    private transactionWatcher: TransactionWatcher;
+    private isInitialised = false;
+
+    constructor(
+        private readonly mxProxy: MXProxyService,
+        private readonly mxApi: MXApiService,
+        private readonly apiConfigService: ApiConfigService,
+        @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+    ) {}
+
+    async onModuleInit(): Promise<void> {
+        const wallet = await promises.readFile(
+            this.apiConfigService.getTaskRunnerWallet(),
+            { encoding: 'utf8' },
+        );
+        const walletObject = JSON.parse(wallet);
+
+        this.accountSigner = UserSigner.fromWallet(
+            walletObject,
+            this.apiConfigService.getTaskRunnerWalletPassword(),
+        );
+
+        this.transactionWatcher = new TransactionWatcher({
+            getTransaction: async (hash) => {
+                return await this.mxProxy
+                    .getService()
+                    .getTransaction(hash, true);
+            },
+        });
+
+        this.isInitialised = true;
+
+        this.logger.info(`The module has been initialized.`, {
+            context: TaskRunnerTransactionService.name,
+        });
+    }
+
+    async broadcastTransaction(tx: TransactionModel): Promise<BroadcastStatus> {
+        this.checkInit();
+
+        try {
+            const { nonce } = await this.mxApi.getAccountStats(
+                this.accountSigner.getAddress().bech32(),
+            );
+
+            const transaction = new Transaction({
+                nonce: BigInt(nonce),
+                sender: tx.sender,
+                receiver: tx.receiver,
+                value: BigInt(tx.value),
+                data: Buffer.from(tx.data, 'base64'),
+                gasPrice: BigInt(tx.gasPrice),
+                gasLimit: BigInt(tx.gasLimit),
+                chainID: tx.chainID,
+                version: 2,
+            });
+
+            transaction.signature = await this.accountSigner.sign(
+                new TransactionComputer().computeBytesForSigning(transaction),
+            );
+
+            const txHash = await this.mxProxy
+                .getService()
+                .sendTransaction(transaction);
+
+            const transactionOnNetwork =
+                await this.transactionWatcher.awaitCompleted(txHash);
+
+            if (!transactionOnNetwork.status.isSuccessful()) {
+                this.logger.error(`Transaction ${txHash} has failed`, {
+                    context: TaskRunnerTransactionService.name,
+                });
+
+                return BroadcastStatus.fail;
+            }
+
+            this.logger.info(`Transaction ${txHash} was successful`, {
+                context: TaskRunnerTransactionService.name,
+            });
+
+            return BroadcastStatus.success;
+        } catch (error) {
+            this.logger.error(error);
+
+            return BroadcastStatus.error;
+        }
+    }
+
+    getSenderAddress(): string {
+        this.checkInit();
+
+        return this.accountSigner.getAddress().toBech32();
+    }
+
+    private checkInit(): void {
+        if (this.isInitialised) {
+            return;
+        }
+
+        const message =
+            'Transaction broadcasting service not initialised, aborting';
+
+        this.logger.warn(message, {
+            context: TaskRunnerTransactionService.name,
+        });
+
+        throw new Error(message);
+    }
+}
