9eab9488dc55f0365e272fa88b22abccb47555e1
hschiau <horia.schiau@multiversx.com>
2025-11-27 19:11:56 +0200

    MEX-923: hash getMany util function (get-or-set mechanism)

--- src/utils/get.many.utils.ts
+++ src/utils/get.many.utils.ts
@@ -31,3 +31,41 @@ export async function getAllKeys<T>(
 
     return values;
 }
+
+export async function getHashFields<T>(
+    cacheService: CacheService,
+    hash: string,
+    fields: string[],
+    getterMethod: (keys: string[]) => Promise<Map<string, T>>,
+): Promise<T[]> {
+    const values = await cacheService.hashGetManyRemote<T>(hash, fields);
+
+    const missingIndexes: number[] = [];
+    const missingFields: string[] = [];
+    values.forEach((value, index) => {
+        if (value === null) {
+            missingFields.push(fields[index]);
+            missingIndexes.push(index);
+        }
+    });
+
+    if (missingFields.length === 0) {
+        return values;
+    }
+
+    const dbResults = await getterMethod(missingFields);
+
+    const hashSetFields: [string, T][] = [];
+    for (const missingIndex of missingIndexes) {
+        const dbValue = dbResults.get(fields[missingIndex]);
+        values[missingIndex] = dbValue;
+
+        if (dbValue !== undefined) {
+            hashSetFields.push([fields[missingIndex], dbValue]);
+        }
+    }
+
+    await cacheService.hashSetManyRemote(hash, hashSetFields);
+
+    return values;
+}
