c23a108237e20eff3790d8616f06324ef4ab5189
hschiau <horia.schiau@multiversx.com>
2026-01-15 12:17:23 +0200

    MEX-940: remove unused rewards compute functions (week APR)

--- src/microservices/dex-state/utils/rewards.compute.utils.ts
+++ src/microservices/dex-state/utils/rewards.compute.utils.ts
@@ -40,80 +40,12 @@ export function computeDistribution(
         const valueUSD = paymentsValueUSD[index];
         const percentage = totalPriceUSD.isZero()
             ? '0.0000'
-            : valueUSD
-                  .dividedBy(totalPriceUSD)
-                  .multipliedBy(100)
-                  .toFixed(4);
+            : valueUSD.dividedBy(totalPriceUSD).multipliedBy(100).toFixed(4);
         return new TokenDistributionModel({
             tokenId: payment.tokenID,
             percentage,
         });
     });
-}
-
-export function computeTotalRewardsForWeekUSD(
-    totalRewardsForWeek: EsdtTokenPayment[],
-    baseAssetTokenId: string,
-    lockedTokenId: string,
-    tokens: Map<string, EsdtToken>,
-): string {
-    return totalRewardsForWeek
-        .reduce((acc, reward) => {
-            const tokenID =
-                reward.tokenID === lockedTokenId
-                    ? baseAssetTokenId
-                    : reward.tokenID;
-
-            const token = tokens.get(tokenID);
-
-            if (!token) {
-                throw new Error(`Token ${reward.tokenID} missing`);
-            }
-
-            const rewardUSD = computeValueUSD(
-                reward.amount,
-                token.decimals,
-                token.price,
-            );
-            return acc.plus(rewardUSD);
-        }, new BigNumber(0))
-        .toFixed();
-}
-
-export function computeWeekAPR(
-    totalLockedTokensForWeek: string,
-    totalRewardsForWeek: EsdtTokenPayment[],
-    baseAssetToken: EsdtToken,
-    lockedTokenId: string,
-    tokens: Map<string, EsdtToken>,
-    scAddressSet: Set<string>,
-): string {
-    if (!scAddressSet.has(baseAssetToken.identifier)) {
-        return '0';
-    }
-
-    const tokenPriceUSD = scAddressSet.has(baseAssetToken.identifier)
-        ? baseAssetToken.price
-        : '0';
-
-    const totalLockedTokensForWeekPriceUSD = new BigNumber(
-        totalLockedTokensForWeek,
-    )
-        .multipliedBy(new BigNumber(tokenPriceUSD))
-        .toFixed();
-
-    const totalRewardsForWeekPriceUSD = computeTotalRewardsForWeekUSD(
-        totalRewardsForWeek,
-        baseAssetToken.identifier,
-        lockedTokenId,
-        tokens,
-    );
-
-    const weekAPR = new BigNumber(totalRewardsForWeekPriceUSD)
-        .times(52)
-        .div(totalLockedTokensForWeekPriceUSD);
-
-    return weekAPR.isNaN() || !weekAPR.isFinite() ? '0' : weekAPR.toFixed();
 }
 
 export function computeBaseRewards(
