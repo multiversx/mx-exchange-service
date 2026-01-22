e077a5deb6759d029610783254de6acfc363af94
hschiau <horia.schiau@multiversx.com>
2026-01-22 18:55:35 +0200

    MEX-943: update state dataloader

--- src/modules/state/state.dataloader.module.ts
+++ src/modules/state/state.dataloader.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TokenModule } from '../tokens/token.module';
 import { StateDataLoader } from './services/state.dataloader';
 import { StateModule } from './state.module';
 
 @Module({
-    imports: [StateModule],
+    imports: [StateModule, TokenModule],
     providers: [StateDataLoader],
     exports: [StateDataLoader],
 })
