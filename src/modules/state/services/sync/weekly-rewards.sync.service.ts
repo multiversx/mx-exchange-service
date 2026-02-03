f72f722ab30348c345f627619b19454a77173e50
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2026-02-03 12:20:55 +0200

    MEX-943: added missing scAddress proto field for weekly rewards
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- src/modules/state/services/sync/weekly-rewards.sync.service.ts
+++ src/modules/state/services/sync/weekly-rewards.sync.service.ts
@@ -47,6 +47,7 @@ export class WeeklyRewardsSyncService {
 
             result.push(
                 new GlobalInfoByWeekModel({
+                    scAddress: address,
                     week,
                     totalRewardsForWeek,
                     totalEnergyForWeek,
